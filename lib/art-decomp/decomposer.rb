58bcf740c3e88688472ea90380d0f7fcaa95f713
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-02-25 00:22:38 +0100

    make the UV/Qu/Qv generators’ architecture symmetric (for now?)

--- lib/art-decomp/decomposer.rb
+++ lib/art-decomp/decomposer.rb
@@ -3,7 +3,9 @@ module ArtDecomp class Decomposer
   def initialize params
     @fsm    = params[:fsm]
     @archs  = params[:archs]
-    @uv_gen = params[:uv_gen].new @fsm, @archs.map{|a| a.pins}.max
+    @uv_gen = params[:uv_gen].new @fsm, @archs
+    @qu_gen = params[:qu_gen].new @fsm, @archs
+    @qv_gen = params[:qv_gen].new @fsm, @archs
   end
 
 end end
--- spec/art-decomp/decomposer_spec.rb
+++ spec/art-decomp/decomposer_spec.rb
@@ -1,11 +1,15 @@
 describe Decomposer do
 
-  it 'should instantiate itself and the given InputSelector generator properly' do
+  it 'should instantiate itself and its components properly' do
     fsm    = mock 'FSM'
     archs  = Set[mock('Arch', :pins => 5), mock('Arch', :pins => 4)] # FIXME unmock
-    uv_gen = mock 'InputSelector class'
-    uv_gen.should_receive(:new).with fsm, 5
-    decomposer = Decomposer.new :fsm => fsm, :archs => archs, :uv_gen => uv_gen
+    uv_gen = mock 'UVGenerator class'
+    qu_gen = mock 'QuGenerator class'
+    qv_gen = mock 'QvGenerator class'
+    uv_gen.should_receive(:new).with fsm, archs
+    qu_gen.should_receive(:new).with fsm, archs
+    qv_gen.should_receive(:new).with fsm, archs
+    decomposer = Decomposer.new :fsm => fsm, :archs => archs, :uv_gen => uv_gen, :qu_gen => qu_gen, :qv_gen => qv_gen
   end
 
 end
