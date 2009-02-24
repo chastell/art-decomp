916d23e2634ab67092ba7eff1f84952838d9955e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-02-24 22:52:29 +0100

    code cleanup/tightening

--- spec/art-decomp/decomposer_spec.rb
+++ spec/art-decomp/decomposer_spec.rb
@@ -2,11 +2,10 @@ describe Decomposer do
 
   it 'should instantiate itself and the given InputSelector generator properly' do
     fsm    = mock 'FSM'
-    arch1  = mock 'Arch', :pins => 5
-    arch2  = mock 'Arch', :pins => 4
+    archs  = Set[mock('Arch', :pins => 5), mock('Arch', :pins => 4)] # FIXME unmock
     uv_gen = mock 'InputSelector class'
     uv_gen.should_receive(:new).with fsm, 5
-    decomposer = Decomposer.new :fsm => fsm, :archs => Set[arch1, arch2], :uv_gen => uv_gen
+    decomposer = Decomposer.new :fsm => fsm, :archs => archs, :uv_gen => uv_gen
   end
 
 end
