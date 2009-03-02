e2b9b406b700a8d162a428e682e9e85f586ae911
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-03-02 23:47:29 +0100

    drop the before block from the Decomposer spec

--- spec/art-decomp/decomposer_spec.rb
+++ spec/art-decomp/decomposer_spec.rb
@@ -1,18 +1,15 @@
 describe Decomposer do
 
-  before do
-    @fsm      = mock 'FSM'
-    @archs    = Set[Arch[5,1], Arch[4,2]]
-  end
-
   it 'should instantiate itself and its components properly' do
-    @uv_class = mock 'UVGenerator class'
-    @qu_class = mock 'QuGenerator class'
-    @qv_class = mock 'QvGenerator class'
-    @uv_class.should_receive(:new).with @fsm, @archs
-    @qu_class.should_receive(:new).with @fsm, @archs
-    @qv_class.should_receive(:new).with @fsm, @archs
-    Decomposer.new :fsm => @fsm, :archs => @archs, :uv_class => @uv_class, :qu_class => @qu_class, :qv_class => @qv_class
+    fsm      = mock 'FSM'
+    archs    = Set[Arch[5,1], Arch[4,2]]
+    uv_class = mock 'UVGenerator class'
+    qu_class = mock 'QuGenerator class'
+    qv_class = mock 'QvGenerator class'
+    uv_class.should_receive(:new).with fsm, archs
+    qu_class.should_receive(:new).with fsm, archs
+    qv_class.should_receive(:new).with fsm, archs
+    Decomposer.new :fsm => fsm, :archs => archs, :uv_class => uv_class, :qu_class => qu_class, :qv_class => qv_class
   end
 
   context 'given that the three generators are working properly' do
@@ -36,19 +33,20 @@ describe Decomposer do
       qv_bA, g_bA = mock('Blanket'), mock('Blanket')
       qv_bB, g_bB = mock('Blanket'), mock('Blanket')
 
-      @uv_class = mock 'UVGenerator class', :new => StubGenerator.new({[] => [[u_a, v_a], [u_b, v_b]]})
-      @qu_class = mock 'QuGenerator class', :new => StubGenerator.new({[u_a, v_a] => [qu_a1, qu_a2],
-                                                                       [u_b, v_b] => [qu_b]})
-      @qv_class = mock 'QvGenerator class', :new => StubGenerator.new({[u_a, v_a, qu_a1] => [[qv_a1, g_a1]],
-                                                                       [u_a, v_a, qu_a2] => [[qv_a2, g_a2]],
-                                                                       [u_b, v_b, qu_b]  => [[qv_bA, g_bA], [qv_bB, g_bB]]})
+      uv_class = mock 'UVGenerator class', :new => StubGenerator.new({[] => [[u_a, v_a], [u_b, v_b]]})
+      qu_class = mock 'QuGenerator class', :new => StubGenerator.new({[u_a, v_a] => [qu_a1, qu_a2],
+                                                                      [u_b, v_b] => [qu_b]})
+      qv_class = mock 'QvGenerator class', :new => StubGenerator.new({[u_a, v_a, qu_a1] => [[qv_a1, g_a1]],
+                                                                      [u_a, v_a, qu_a2] => [[qv_a2, g_a2]],
+                                                                      [u_b, v_b, qu_b]  => [[qv_bA, g_bA], [qv_bB, g_bB]]})
 
-      decomposer = Decomposer.new :fsm => @fsm, :archs => @archs, :uv_class => @uv_class, :qu_class => @qu_class, :qv_class => @qv_class
+      fsm = mock 'FSM'
+      decomposer = Decomposer.new :fsm => fsm, :uv_class => uv_class, :qu_class => qu_class, :qv_class => qv_class
       results = []
       decomposer.each { |dec| results << dec }
       results.size.should  == 4
-      results.first.should == Decomposition.new(@fsm, u_a, v_a, qu_a1, qv_a1, g_a1)
-      results.last.should  == Decomposition.new(@fsm, u_b, v_b, qu_b,  qv_bB, g_bB)
+      results.first.should == Decomposition.new(fsm, u_a, v_a, qu_a1, qv_a1, g_a1)
+      results.last.should  == Decomposition.new(fsm, u_b, v_b, qu_b,  qv_bB, g_bB)
     end
 
   end
