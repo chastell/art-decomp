a6aa0ff78c74ba7ebb2ae3e4ace3e46d96d58ea5
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-03-03 20:32:27 +0100

    tighter Arch#== and Decomposition#== specs

--- spec/art-decomp/arch_spec.rb
+++ spec/art-decomp/arch_spec.rb
@@ -2,6 +2,8 @@ describe Arch do
 
   it 'should be instantiable Array-style (and compare properly)' do
     Arch[5,1].should == Arch.new(5, 1)
+    Arch[5,2].should_not == Arch[5,1]
+    Arch[4,1].should_not == Arch[5,1]
   end
 
 end
--- spec/art-decomp/decomposition_spec.rb
+++ spec/art-decomp/decomposition_spec.rb
@@ -5,8 +5,9 @@ describe Decomposition do
     u, v = [0,1], [2]
     qu, qv, g = mock('Blanket'), mock('Blanket'), mock('Blanket')
     dec = Decomposition.new fsm, u, v, qu, qv, g
-    [dec.fsm, dec.u, dec.v, dec.qu, dec.qv, dec.g].should == [fsm, u, v, qu, qv, g]
-    dec.should == Decomposition.new(fsm, u, v, qu, qv, g)
+    dec.should     == Decomposition.new(fsm, u, v, qu, qv, g)
+    dec.should_not == Decomposition.new(fsm, v, u, qu, qv, g)
+    dec.should_not == Decomposition.new(fsm, u, v, qv, qu, g)
   end
 
 end
