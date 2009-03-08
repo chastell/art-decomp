3a301af5309bd290dd3b37735adc86d4db3036fc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-03-08 20:49:56 +0100

    FSM spec: current specs’ values for the new FSMs

--- spec/art-decomp/fsm_spec.rb
+++ spec/art-decomp/fsm_spec.rb
@@ -4,14 +4,20 @@ describe FSM do
 
     before do
       @opus = FSM.from_kiss 'spec/fixtures/opus'
+      @lion = FSM.from_kiss 'spec/fixtures/lion'
+      @mc   = FSM.from_kiss 'spec/fixtures/mc'
     end
 
     it 'should properly report the number of inputs' do
       @opus.input_count.should == 5
+      @lion.input_count.should == 2
+      @mc.input_count.should   == 3
     end
 
     it 'should properly generate the βq blanket' do
       @opus.beta_q.should == Blanket[B[0,1,2], B[0,3,4], B[0,5], B[0,6,7], B[0,8,9,10,11,12,13,14], B[0,15,16], B[0,17,18], B[0,19], B[0,20], B[0,21]]
+      @lion.beta_q.should == Blanket[B[0,1,2], B[3,4,5], B[6,7,8], B[9,10]]
+      @mc.beta_q.should   == Blanket[B[0,1,2], B[3,4], B[5,6,7], B[8,9]]
     end
 
   end
