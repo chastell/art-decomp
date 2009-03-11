ad01124c74ba223d4fef630f3ebd2a2d85a33d0f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-03-11 18:49:51 +0100

    typo fixes

--- spec/art-decomp/fsm_spec.rb
+++ spec/art-decomp/fsm_spec.rb
@@ -14,13 +14,13 @@ describe FSM do
       @mc.input_count.should   == 3
     end
 
-    it 'should properly generate the βq blanket' do
+    it 'should properly generate the Q Blanket' do
       @opus.beta_q.should == Blanket[B[0,1,2], B[0,3,4], B[0,5], B[0,6,7], B[0,8,9,10,11,12,13,14], B[0,15,16], B[0,17,18], B[0,19], B[0,20], B[0,21]]
       @lion.beta_q.should == Blanket[B[0,1,2], B[3,4,5], B[6,7,8], B[9,10]]
       @mc.beta_q.should   == Blanket[B[0,1,2], B[3,4], B[5,6,7], B[8,9]]
     end
 
-    it 'should properly generate the βf blanket' do
+    it 'should properly generate the F Blanket' do
       @opus.beta_f.should == Blanket[B[0,1], B[2,3,9], B[4,14], B[5,6], B[7,8,20,21], B[10,16], B[11,18], B[12,15], B[13,17], B[19]]
       @lion.beta_f.should == Blanket[B[0,1,4], B[2], B[2,3,7], B[5,6,10], B[8,9]]
       @mc.beta_f.should   == Blanket[B[0,1], B[2], B[3], B[4], B[5], B[6,7], B[8], B[9]]
