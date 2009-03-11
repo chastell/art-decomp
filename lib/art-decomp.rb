c12046e6ed53e1c31e991f5abd9e9f790c4eb581
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-03-11 21:52:34 +0100

    use require_relative instead of mucking with $LOAD_PATH

--- lib/art-decomp.rb
+++ lib/art-decomp.rb
@@ -1,18 +1,16 @@
-$LOAD_PATH.unshift File.dirname(__FILE__)
-
 module ArtDecomp
   DontCare = :-
 end
 
 require 'set'
 
-require 'enumerable'
-require 'integer'
-require 'art-decomp/arch'
-require 'art-decomp/b'
-require 'art-decomp/blanket'
-require 'art-decomp/decomposer'
-require 'art-decomp/decomposition'
-require 'art-decomp/fsm'
-require 'art-decomp/sep'
-require 'art-decomp/uv_generator/braindead'
+require_relative 'enumerable'
+require_relative 'integer'
+require_relative 'art-decomp/arch'
+require_relative 'art-decomp/b'
+require_relative 'art-decomp/blanket'
+require_relative 'art-decomp/decomposer'
+require_relative 'art-decomp/decomposition'
+require_relative 'art-decomp/fsm'
+require_relative 'art-decomp/sep'
+require_relative 'art-decomp/uv_generator/braindead'
