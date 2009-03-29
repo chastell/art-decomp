2737ec3d15fba11d7bba709fd60fe9600d3cc85c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-03-29 23:21:07 +0200

    refactor Graph to be unlabeled for now

--- lib/art-decomp/graph.rb
+++ lib/art-decomp/graph.rb
@@ -1,26 +1,19 @@
 module ArtDecomp class Graph
 
+  attr_reader :edges, :vertices
+
   def initialize blanket, separations
-    @vertices = {}
-    @edges    = {}
-    blanket.ints.each { |i| @vertices[i] = nil }
+    @vertices = blanket.ints.dup
+    @edges    = Set[]
     separations.each do |sep|
-      vertices.every_pair do |a, b|
-        @edges[Set[a,b]] = nil unless (a & sep).zero? or (b & sep).zero? or (a | b) & sep != sep
+      @vertices.every_pair do |a, b|
+        @edges << Set[a,b] unless (a & sep).zero? or (b & sep).zero? or (a | b) & sep != sep
       end
     end
   end
 
   def degree vertex
-    edges.select { |e| e.include? vertex }.size
-  end
-
-  def edges
-    @edges.keys.to_set
-  end
-
-  def vertices
-    @vertices.keys.to_set
+    @edges.select { |e| e.include? vertex }.size
   end
 
 end end
