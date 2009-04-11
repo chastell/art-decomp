d1bc9231a9eb40868868c83d5d9066bea0710bc2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-04-12 01:24:55 +0200

    make Enumerator#pairs simply an enum for #every_pair

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,6 +1,7 @@
 module Enumerable
 
   def every_pair
+    # FIXME: use #combination where feasible
     each_with_index do |a, i|
       each_with_index do |b, j|
         yield a, b if i < j
@@ -9,14 +10,8 @@ module Enumerable
   end
 
   def pairs
-    return combination(2) if respond_to? :combination
-    Enumerator.new do |yielder|
-      each_with_index do |a, i|
-        each_with_index do |b, j|
-          yielder.yield a, b if i < j
-        end
-      end
-    end
+    # FIXME: use #combination where feasible
+    enum_for :every_pair
   end
 
 end
--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,21 +1,15 @@
 describe Enumerable do
 
-  it 'should provide consecutive element pairs' do
+  it 'should provide consecutive element pairs when called with a block' do
     array = [5,1,3,4]
     result = []
     array.every_pair { |a,b| result << [a,b] }
     result.should == [[5,1], [5,3], [5,4], [1,3], [1,4], [3,4]]
   end
 
-  it 'should provide consecutive element pairs' do
+  it 'should provide consecutive element pairs when called as an Enumerator' do
     set = Set[1,2,3,4]
     set.pairs.to_a.should == [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
-  end
-
-  it 'should piggyback Array#combination where possible' do
-    array = [1,2,3,4]
-    array.should_receive(:combination).with 2
-    array.pairs
   end
 
 end
