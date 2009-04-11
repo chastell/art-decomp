137168c86df5130fbe8436fd9606d0f10d7bfe4c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-04-12 00:50:46 +0200

    add Enumerable#pairs

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -8,4 +8,15 @@ module Enumerable
     end
   end
 
+  def pairs
+    return combination(2) if respond_to? :combination
+    Enumerator.new do |yielder|
+      each_with_index do |a, i|
+        each_with_index do |b, j|
+          yielder.yield a, b if i < j
+        end
+      end
+    end
+  end
+
 end
--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -7,4 +7,15 @@ describe Enumerable do
     result.should == [[5,1], [5,3], [5,4], [1,3], [1,4], [3,4]]
   end
 
+  it 'should provide consecutive element pairs' do
+    set = Set[1,2,3,4]
+    set.pairs.to_a.should == [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
+  end
+
+  it 'should piggyback Array#combination where possible' do
+    array = [1,2,3,4]
+    array.should_receive(:combination).with 2
+    array.pairs
+  end
+
 end
