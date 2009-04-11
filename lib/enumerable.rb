91aa5b230b77d28a8f5d3fe0fc84b3b721b154de
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-04-12 01:59:13 +0200

    make Enumerable#every_pair use #pairs, not the other way around

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,17 +1,19 @@
 module Enumerable
 
-  def every_pair
+  def every_pair &block
     # FIXME: use #combination where feasible
-    each_with_index do |a, i|
-      each_with_index do |b, j|
-        yield a, b if i < j
-      end
-    end
+    pairs.each &block
   end
 
   def pairs
     # FIXME: use #combination where feasible
-    enum_for :every_pair
+    Enumerator.new do |yielder|
+      each_with_index do |a, i|
+        each_with_index do |b, j|
+          yielder.yield a, b if i < j
+        end
+      end
+    end
   end
 
 end
