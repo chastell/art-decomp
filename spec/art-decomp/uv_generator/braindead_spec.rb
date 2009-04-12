c054d2b4ea987cb15dc6cc0e9535ead064b65aa4
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-04-12 18:47:02 +0200

    minor spec rename to avoid confusion with Enumerable#pairs

--- spec/art-decomp/uv_generator/braindead_spec.rb
+++ spec/art-decomp/uv_generator/braindead_spec.rb
@@ -6,12 +6,12 @@ describe UVGenerator::Braindead do
       fsm = mock 'FSM', :input_count => 4
       archs = Set[Arch[3,1]]
       uv_gen = UVGenerator::Braindead.new fsm, archs
-      pairs = []
-      uv_gen.each { |u, v| pairs << [u, v] }
-      pairs.size.should  == 15
-      pairs.first.should == [[0,1,2,3], []]
-      pairs[7].should    == [[3], [0,1,2]]
-      pairs.last.should  == [[0], [1,2,3]]
+      uvs = []
+      uv_gen.each { |u, v| uvs << [u, v] }
+      uvs.size.should  == 15
+      uvs.first.should == [[0,1,2,3], []]
+      uvs[7].should    == [[3], [0,1,2]]
+      uvs.last.should  == [[0], [1,2,3]]
     end
 
   end
