05b6fb84d6865c474ccff1a2531d0260af58d146
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-04-19 21:33:29 +0200

    simplify Set#r_adm

--- lib/set.rb
+++ lib/set.rb
@@ -1,20 +1,14 @@
 class Set
 
-  # FIXME: consider an algorithm with lesser complexity
   def r_adm int
+    # FIXME: consider an algorithm with lesser complexity
     seps = select { |sep| int & sep == sep }
     subblocks = Set[]
     int.bits.each do |elem|
-      placed = false
-      subblocks.each do |subblock|
-        extended = subblock + [elem]
-        unless seps.any? { |sep| ArtDecomp::B[*extended] & sep == sep }
-          subblock << elem
-          placed = true
-          break
-        end
-      end
-      subblocks << [elem] unless placed
+      bit = 1 << elem
+      subblock = subblocks.find { |sb| seps.none? { |sep| (sb | bit) & sep == sep } } || 0
+      subblocks.delete subblock
+      subblocks << (subblock | bit)
     end
     subblocks.size.log2_ceil
   end
--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -7,7 +7,8 @@ describe Set, 'of Separations' do
     Set[Sep[1,2], Sep[5,6]].r_adm(b).should == 1
     Set[Sep[1,2], Sep[3,4]].r_adm(b).should == 1
     Set[Sep[1,3], Sep[1,4], Sep[2,3], Sep[2,4]].r_adm(b).should == 1
-    Set[Sep[9,11], Sep[8,13], Sep[7,13], Sep[10,12], Sep[13,17], Sep[8,17], Sep[10,16]].r_adm(B[7,8,9,10,11,12,13,16,17]).should == 2
+    Set[Sep[1,3], Sep[1,4], Sep[2,3], Sep[2,4], Sep[3,4]].r_adm(b).should == 2
+    Blanket[B[1],B[2],B[3],B[4]].seps.r_adm(b).should == 2
   end
 
 end
