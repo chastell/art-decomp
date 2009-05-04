e3970ef29f9a784d6156f4519458ac13e32114ea
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-05-04 20:41:58 +0200

    add QvGenerator::Bipainting

---
+++ lib/art-decomp/qv_generator/bipainting.rb
@@ -0,0 +1,14 @@
+module ArtDecomp::QvGenerator class Bipainting
+
+  def initialize fsm, archs
+    @fsm, @beta_f, @beta_q = fsm, fsm.beta_f, fsm.beta_q
+  end
+
+  def each u, v, beta_qu
+    beta_u = @fsm.beta_x u
+    beta_v = @fsm.beta_x v
+    bipainter = Bipainter.new @beta_q, beta_v, @beta_f.seps - beta_u.seps - beta_qu.seps
+    yield bipainter.blankets
+  end
+
+end end
