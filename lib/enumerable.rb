e05c36c081a4d1b4c9e80f5baca253acc65c72cc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-05-19 23:31:36 +0200

    add Enumerable.all_combinations

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,5 +1,22 @@
 module Enumerable
 
+  def self.all_combinations source
+    result_count = source.map(&:size).inject(:*)
+    group_count = 1
+    result = []
+    source.each do |elems|
+      row = []
+      group_count.times do
+        elems.each do |elem|
+          (result_count / group_count / elems.size).times { row << elem }
+        end
+      end
+      group_count *= elems.size
+      result << row
+    end
+    result.transpose
+  end
+
   def pairs
     return combination 2 if respond_to? :combination
     Enumerator.new do |yielder|
