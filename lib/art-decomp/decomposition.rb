9423a22eca48931bf892ac8f97cb1b762eb06cc8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-05-20 02:20:38 +0200

    add Decomposition#g_kiss

--- lib/art-decomp/decomposition.rb
+++ lib/art-decomp/decomposition.rb
@@ -8,6 +8,16 @@ module ArtDecomp class Decomposition
     [@fsm, @u, @v, @qu, @qv, @g] == [other.fsm, other.u, other.v, other.qu, other.qv, other.g]
   end
 
+  def g_kiss
+    lines = []
+    (@fsm.beta_x(@v) * @qv).ints.each do |row|
+      Enumerable.all_combinations(@v.map { |i| @fsm.x_encodings(i, row) } + [@qv.encodings(row)]).each do |encs|
+        lines << encs.join + ' ' + @g.encodings(row).first
+      end
+    end
+    lines.sort.uniq.join("\n") + "\n"
+  end
+
   def hash
     @fsm.hash ^ @u.hash ^ @v.hash ^ @qu.hash ^ @qv.hash ^ @g.hash
   end
--- spec/art-decomp/decomposition_spec.rb
+++ spec/art-decomp/decomposition_spec.rb
@@ -11,4 +11,13 @@ describe Decomposition do
     dec.hash.should == Decomposition.new(fsm, u, v, qu, qv, g).hash
   end
 
+  it 'should create its G block’s KISS representation properly' do
+    fsm = FSM.from_kiss 'spec/fixtures/fsm'
+    qu  = Blanket[B[0,1,2,3,4,5,17], B[6,7,8,9,10,11,12,13,14,15,16,18,19]]
+    qv  = Blanket[B[0,1,2,17,19], B[3,6,7,8,9,10,11,12], B[4,5,18], B[13,14,15,16]]
+    g   = Blanket[B[0,1,4,6,17,18,19], B[2,7,9,11], B[3,5,8,10,12], B[13,14,15,16]]
+    decomposition = Decomposition.new fsm, [1,3], [0,2], qu, qv, g
+    decomposition.g_kiss.should == File.read('spec/fixtures/fsm.g')
+  end
+
 end
