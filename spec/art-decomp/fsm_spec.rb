dd5fb6c14938b2b332dff233ccf9f08e30a85303
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-05-22 01:22:01 +0200

    simplify FSM spec

--- spec/art-decomp/fsm_spec.rb
+++ spec/art-decomp/fsm_spec.rb
@@ -41,9 +41,6 @@ describe FSM do
       @opus.x_encoding(2, B[0]).should   == :'1'
       @opus.x_encoding(0, B[0]).should   == DontCare
       @opus.x_encoding(0, B[7,8]).should == :'0'
-    end
-
-    it 'should raise an exception if it’s asked about ambiguous encoding' do
       lambda { @opus.x_encoding 0, B[8,9] }.should raise_error(AmbiguousEncodingQuery, 'ambiguous encoding query: input 0, block 8,9')
     end
 
