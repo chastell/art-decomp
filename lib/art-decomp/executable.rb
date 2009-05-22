13e97b97907cfdd27133925c25388a5bfe6fbf5c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-05-22 23:54:22 +0200

    æsthetic fixes

--- lib/art-decomp/executable.rb
+++ lib/art-decomp/executable.rb
@@ -3,7 +3,7 @@ require 'trollop'
 module ArtDecomp class Executable
 
   def initialize args = ARGV
-    opts = Trollop::options(args) do
+    opts = Trollop.options(args) do
       opt :arch,   'Target architecture', :type => :string, :multi => true
       opt :outdir, 'Output directory',    :type => :string
       opt :uv,     'UV generator',        :default => 'Braindead'
@@ -11,17 +11,17 @@ module ArtDecomp class Executable
       opt :qv,     'Qv generator',        :default => 'GraphColouring'
     end
 
-    Trollop::die          'no FSM given'                      if     args.empty?
-    Trollop::die          'FSM does not exist'                unless File.exists? args.first
-    Trollop::die          'no architecture given'             unless opts[:arch_given]
-    Trollop::die :arch,   'not in the form of inputs/outputs' unless opts[:arch].all? { |s| s =~ /^\d+\/\d+$/ }
-    Trollop::die :outdir, 'no output directory given'         unless opts[:outdir_given]
-    Trollop::die :outdir, 'output directory exists'           if     File.exists? opts[:outdir]
-    Trollop::die :uv,     'no such UV generator'              unless UVGenerator.constants.include? opts[:uv].to_sym
-    Trollop::die :qu,     'no such Qu generator'              unless QuGenerator.constants.include? opts[:qu].to_sym
-    Trollop::die :qv,     'no such Qv generator'              unless QvGenerator.constants.include? opts[:qv].to_sym
+    Trollop.die          'no FSM given'                      if     args.empty?
+    Trollop.die          'FSM does not exist'                unless File.exists? args.first
+    Trollop.die          'no architecture given'             unless opts[:arch_given]
+    Trollop.die :arch,   'not in the form of inputs/outputs' unless opts[:arch].all? { |s| s =~ /^\d+\/\d+$/ }
+    Trollop.die :outdir, 'no output directory given'         unless opts[:outdir_given]
+    Trollop.die :outdir, 'output directory exists'           if     File.exists? opts[:outdir]
+    Trollop.die :uv,     'no such UV generator'              unless UVGenerator.constants.include? opts[:uv].to_sym
+    Trollop.die :qu,     'no such Qu generator'              unless QuGenerator.constants.include? opts[:qu].to_sym
+    Trollop.die :qv,     'no such Qv generator'              unless QvGenerator.constants.include? opts[:qv].to_sym
 
-    Dir.mkdir opts[:outdir] rescue SystemCallError Trollop::die :outdir, 'output directory cannot be created'
+    Dir.mkdir opts[:outdir] rescue SystemCallError Trollop.die :outdir, 'output directory cannot be created'
 
     @dir   = opts[:outdir]
     @fsm   = FSM.from_kiss args.first
@@ -37,10 +37,8 @@ module ArtDecomp class Executable
     decs = []
     decomposer.each.with_index do |dec, i|
       decs << dec
-      if dump_tables
-        File.write_data File.join(@dir, "#{i}.g"), dec.g_kiss
-        File.write_data File.join(@dir, "#{i}.h"), dec.h_kiss
-      end
+      File.write_data File.join(@dir, "#{i}.g"), dec.g_kiss if dump_tables
+      File.write_data File.join(@dir, "#{i}.h"), dec.h_kiss if dump_tables
     end
 
     File.dump_object File.join(@dir, 'decompositions'), decs
