8eeb6078c2a5c42e3e72f82e3f50d6fde9b70e92
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-08-16 13:22:36 +0200

    simplify File.dump_object

--- lib/file.rb
+++ lib/file.rb
@@ -1,11 +1,11 @@
 class File
 
   def self.dump_object path, object
-    File.open(path, 'w') { |f| f << Marshal.dump(object) }
+    write_data path, Marshal.dump(object)
   end
 
   def self.write_data path, data
-    File.open(path, 'w') { |f| f << data }
+    self.open(path, 'w') { |f| f << data }
   end
 
 end
