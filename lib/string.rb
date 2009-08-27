113b5e6d00222347cb88b4d65f1040a5647a4e5a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-08-27 21:17:31 +0200

    fix String#dc_expand

--- lib/string.rb
+++ lib/string.rb
@@ -4,7 +4,7 @@ class String
     return [self] unless include? ArtDecomp::DontCare
     offsets = columns.dup
     i = index ArtDecomp::DontCare.to_s, offsets.shift until columns.include? i or offsets.empty?
-    return [self] if i.nil?
+    return [self] unless columns.include? i
     zero,    one    = dup, dup
     zero[i], one[i] = '0', '1'
     [zero.dc_expand(columns), one.dc_expand(columns)].flatten
