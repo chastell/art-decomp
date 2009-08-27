3a7099eaff3b0fabbce255b8b07918abee058bba
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-08-27 21:07:33 +0200

    teach String#dc_expand to expand only selected columns

--- lib/string.rb
+++ lib/string.rb
@@ -1,11 +1,13 @@
 class String
 
-  def dc_expand
+  def dc_expand columns = (0...size).to_a
     return [self] unless self.include? ArtDecomp::DontCare
-    i = self.index ArtDecomp::DontCare.to_s
+    offsets = columns.dup
+    i = self.index ArtDecomp::DontCare.to_s, offsets.shift until columns.include? i or offsets.empty?
+    return [self] if i.nil?
     zero = self[0...i] + '0' + self[i+1..-1]
     one  = self[0...i] + '1' + self[i+1..-1]
-    [zero.dc_expand, one.dc_expand].flatten
+    [zero.dc_expand(columns), one.dc_expand(columns)].flatten
   end
 
 end
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -6,4 +6,9 @@ describe String do
     '-1-'.dc_expand.should == ['010', '011', '110', '111']
   end
 
+  it 'the expansion should be limitable to just a given set of columns' do
+    '---'.dc_expand.should == ['000', '001', '010', '011', '100', '101', '110', '111']
+    '---'.dc_expand([0,2]).should == ['0-0', '0-1', '1-0', '1-1']
+  end
+
 end
