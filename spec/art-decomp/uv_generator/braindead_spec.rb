aae64dfbde5191aa2e2e2b089c17a9be4d39b05b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-08-27 15:15:06 +0200

    make UVGenerator::Braindead#each expand FSM’s V colmns

--- spec/art-decomp/uv_generator/braindead_spec.rb
+++ spec/art-decomp/uv_generator/braindead_spec.rb
@@ -4,6 +4,7 @@ module ArtDecomp describe UVGenerator::Braindead do
 
     it 'should yield all non-insane U and V combinations' do
       fsm = mock 'FSM', :input_count => 4
+      fsm.stub!(:expand_v).and_return fsm
       archs = Set[Arch[3,1]]
       uv_gen = UVGenerator::Braindead.new fsm, archs
       uvs = []
@@ -12,6 +13,22 @@ module ArtDecomp describe UVGenerator::Braindead do
       uvs.first.should == [fsm, [0,1,2,3], []]
       uvs[7].should    == [fsm, [3], [0,1,2]]
       uvs.last.should  == [fsm, [0], [1,2,3]]
+    end
+
+    it 'should yield V-expanded FSMs' do
+      fsm = mock 'FSM', :input_count => 2
+      fsm0, fsm1, fsm2, fsm3 = mock('FSM'), mock('FSM'), mock('FSM'), mock('FSM')
+      fsm.should_receive(:expand_v).exactly(4).times.and_return(fsm0, fsm1, fsm2, fsm3)
+      archs = Set[Arch[3,1]]
+      uv_gen = UVGenerator::Braindead.new fsm, archs
+      uvs = []
+      uv_gen.each { |f, u, v| uvs << [f, u, v] }
+      uvs.should == [
+        [fsm0, [0,1], []],
+        [fsm1, [1],   [0]],
+        [fsm2, [0],   [1]],
+        [fsm3, [],    [0,1]],
+      ]
     end
 
   end
