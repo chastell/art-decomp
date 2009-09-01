ee83bb4e231e4c3c327113c2b218fd70d4765a57
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-09-01 02:45:55 +0200

    adjust Decomposition spec to use only disjoint beta_v cases

--- spec/art-decomp/decomposition_spec.rb
+++ spec/art-decomp/decomposition_spec.rb
@@ -13,10 +13,10 @@ module ArtDecomp describe Decomposition do
 
   it 'should create its F, Q, G and H blocks’ KISS representations properly' do
     fsm = FSM.from_kiss 'spec/fixtures/fsm'
-    qu  = Blanket[B[0,1,2,3,4,5,17], B[6,7,8,9,10,11,12,13,14,15,16,18,19]]
-    qv  = Blanket[B[0,1,2,17,19], B[3,6,7,8,9,10,11,12], B[4,5,18], B[13,14,15,16]]
-    g   = Blanket[B[0,1,4,6,17,18,19], B[2,7,9,11], B[3,5,8,10,12], B[13,14,15,16]]
-    decomposition = Decomposition.new fsm, [1,3], [0,2], qu, qv, g
+    qu  = Blanket[B[0,4,5], B[1,2,3,13,14], B[6,7,8,9,10,11,12], B[15,16,17,18,19]]
+    qv  = Blanket[B[0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19], B[1,2,18]]
+    g   = Blanket[B[0,2,5,6,7,9,11,15,16,17,18,19], B[1,3,4,8,10,12,13,14]]
+    decomposition = Decomposition.new fsm, [0,1,3], [2], qu, qv, g
     decomposition.f_kiss.should == File.read('spec/fixtures/fsm.f')
     decomposition.q_kiss.should == File.read('spec/fixtures/fsm.q')
     decomposition.g_kiss.should == File.read('spec/fixtures/fsm.g')
@@ -24,20 +24,20 @@ module ArtDecomp describe Decomposition do
   end
 
   it 'should create the H block properly, even if the G blanket seems ambiguous' do
-    fsm = FSM.from_kiss 'spec/fixtures/lion'
-    qu  = Blanket[B[0,1,2,3,4,5], B[6,7,8,9,10]]
-    qv  = Blanket[B[0,1,2], B[3,4,5], B[6,7,8], B[9,10]]
-    g   = Blanket[B[0,1,7,8], B[0,2,9], B[3], B[4,5,6,10]]
-    decomposition = Decomposition.new fsm, [1], [0], qu, qv, g
-    decomposition.h_kiss.should == File.read('spec/fixtures/lion.h')
+    fsm = FSM.from_kiss 'spec/fixtures/opus'
+    qu  = Blanket[B[0,1,11,14,15,16,17,18,19,20,30,33,34,35,36,37], B[2,5,7,14,21,24,26,33], B[3,4,10,13,14,22,23,29,32,33], B[6,8,9,12,14,25,27,28,31,33]]
+    qv  = Blanket[B[0,2,6,8,10,13,14,15,19,21,25,27,29,32,33,34], B[1,3,4,5,7,9,12,14,20,22,23,24,26,28,31,33], B[11,14,16,17,18,30,33,35,36,37]]
+    g   = Blanket[B[0,2,6,8,10,13,14,15,19,21,25,27,29,32,33,34], B[1,3,4,5,7,9,12,14,20,22,23,24,26,28,31,33], B[11,14,16,17,18], B[30,33,35,36,37]]
+    decomposition = Decomposition.new fsm.expand_x([0]), [1,2,3,4], [0], qu, qv, g
+    decomposition.h_kiss.should == File.read('spec/fixtures/opus.amb.h')
   end
 
   it 'should properly recode don’t-care states to *' do
     fsm = FSM.from_kiss 'spec/fixtures/opus'
     qu  = Blanket[B[0,1,2,3,4,20,21], B[0,5,6,7], B[0,8,9,10,11,12,13,14,15,16], B[0,17,18,19]]
-    qv  = Blanket[B[0,1,2,6,7,8,9,10,11,12,13,14,17,18], B[0,3,4,5,15,16,19], B[0,20,21]]
-    g   = Blanket[B[0,1], B[2,6,7,8,10,12,14,17,18], B[2,6,7,9,11,13,14,17,18], B[3,4,5,15,16,19], B[20,21]]
-    decomposition = Decomposition.new fsm, [1,3,4], [0,2], qu, qv, g
+    qv  = Blanket[B[0,1,2,6,7,15,16,17,18], B[0,3,4,5,8,9,10,11,12,13,14,19], B[0,20,21]]
+    g   = Blanket[B[0,1], B[2,6,7,15,16,17,18], B[3,4,5,8,9,10,11,12,13,14,19], B[20,21]]
+    decomposition = Decomposition.new fsm, [0,1,3,4], [2], qu, qv, g
     decomposition.h_kiss.should == File.read('spec/fixtures/opus.h')
   end
 
