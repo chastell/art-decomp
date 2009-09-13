51e5b313728281d6bc72fc6a78c4fce31039afe2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-09-13 14:25:49 +0200

    add logging to all generators

--- lib/art-decomp/logging.rb
+++ lib/art-decomp/logging.rb
@@ -10,6 +10,7 @@ module ArtDecomp class Logging < Gazer::Aspect::Base
   end
 
   def self.off
+    # FIXME: if instances can be unadvised, do that and close @@log
     @@log = Logger.new '/dev/null'
     Decomposer.unadvise_all
   end
@@ -18,8 +19,16 @@ module ArtDecomp class Logging < Gazer::Aspect::Base
     @@log.info "decomposing a #{point.args.first[:fsm].stats} FSM into #{point.args.first[:archs].map(&:to_s).sort.reverse.join '+'} cells"
   end
 
+  before instances_of(UVGenerator.constants.map { |c| eval("UVGenerator::#{c}") }) => :uv_pairs do |point|
+    @@log.info "generating UV pairs with #{point.object.class}"
+  end
+
   before instances_of(QuGenerator.constants.map { |c| eval("QuGenerator::#{c}") }) => :blankets do |point|
-    @@log.info "U = #{point.args[1]}, V = #{point.args[2]}"
+    @@log.info "U = #{point.args[1]}, V = #{point.args[2]}, generating Qu blankets with #{point.object.class}"
+  end
+
+  before instances_of(QvGenerator.constants.map { |c| eval("QvGenerator::#{c}") }) => :blankets do |point|
+    @@log.info "|Qu| = #{point.args[3].size}, generating Qv+G blankets with #{point.object.class}"
   end
 
 end end
--- spec/art-decomp/logging_spec.rb
+++ spec/art-decomp/logging_spec.rb
@@ -23,13 +23,28 @@ module ArtDecomp describe Logging do
     log.should =~ /^20..-..-.. ..:..:.. ....0 decomposing a 4\/2, 10s FSM into 5\/1\+4\/2 cells$/
   end
 
+  it 'should log UVGenerators’ uv_pairs calls' do
+    uv = UVGenerator::Braindead.new mock(FSM, :input_count => 2), Set[Arch[5,1]]
+    uv.uv_pairs
+    log.should =~ /generating UV pairs with ArtDecomp::UVGenerator::Braindead/
+  end
+
   it 'should log QuGenerators’ blankets calls' do
     qu = QuGenerator::BlockTable.new
     [[[0], [1]], [[1], [0]]].each do |u, v|
       qu.blankets mock(FSM), u, v
     end
-    log.should =~ /U = \[0\], V = \[1\]/
-    log.should =~ /U = \[1\], V = \[0\]/
+    log.should =~ /U = \[0\], V = \[1\], generating Qu blankets with ArtDecomp::QuGenerator::BlockTable/
+    log.should =~ /U = \[1\], V = \[0\], generating Qu blankets with ArtDecomp::QuGenerator::BlockTable/
+  end
+
+  it 'should log QvGenerators’ blankets calls' do
+    qv = QvGenerator::GraphColouring.new
+    [mock(Blanket, :size => 8), mock(Blanket, :size => 4)].each do |qu|
+      qv.blankets mock(FSM), [0], [1], qu
+    end
+    log.should =~ /\|Qu\| = 8, generating Qv\+G blankets with ArtDecomp::QvGenerator::GraphColouring/
+    log.should =~ /\|Qu\| = 4, generating Qv\+G blankets with ArtDecomp::QvGenerator::GraphColouring/
   end
 
 end end
