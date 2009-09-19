bdeea4146d8462194b3b7b78927066db2e3d79f2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-09-19 14:32:34 +0200

    make Decomposer#decomposition simply not yield already-seen decomps

--- lib/art-decomp/decomposer.rb
+++ lib/art-decomp/decomposer.rb
@@ -8,29 +8,28 @@ module ArtDecomp class Decomposer
   end
 
   def decompositions
-    # FIXME: introduce caching to avoid recomputation when different generators produce identical output
-    cache = {}
+    seen = {}
     Enumerator.new do |yielder|
       @uv_gens.each do |uv_gen|
         uv_gen.uv_pairs.each do |fsm, u, v|
-          @qu_gens.each do |qu_gen|
-            qu_gen.blankets(fsm, u, v).each do |qu|
-              if cache[[fsm, u, v, qu]]
-                cache[[fsm, u, v, qu]].each do |qv, g, qv_gen_class|
-                  dec = Decomposition.new fsm, u, v, qu, qv, g, :uv_gen => uv_gen.class, :qu_gen => qu_gen.class, :qv_gen => qv_gen_class
-                  yielder.yield dec if dec.sensible? @archs
-                end
-              else
-                cache[[fsm, u, v, qu]] = []
-                @qv_gens.each do |qv_gen|
-                  qv_gen.blankets(fsm, u, v, qu).each do |qv, g|
-                    dec = Decomposition.new fsm, u, v, qu, qv, g, :uv_gen => uv_gen.class, :qu_gen => qu_gen.class, :qv_gen => qv_gen.class
-                    yielder.yield dec if dec.sensible? @archs
-                    cache[[fsm, u, v, qu]] << [qv, g, qv_gen.class]
+          unless seen[[fsm, u, v]]
+            @qu_gens.each do |qu_gen|
+              qu_gen.blankets(fsm, u, v).each do |qu|
+                unless seen[[fsm, u, v, qu]]
+                  @qv_gens.each do |qv_gen|
+                    qv_gen.blankets(fsm, u, v, qu).each do |qv, g|
+                      unless seen[[fsm, u, v, qu, qv, g]]
+                        dec = Decomposition.new fsm, u, v, qu, qv, g, :uv_gen => uv_gen.class, :qu_gen => qu_gen.class, :qv_gen => qv_gen.class
+                        yielder.yield dec if dec.sensible? @archs
+                        seen[[fsm, u, v, qu, qv, g]] = true
+                      end
+                    end
                   end
+                  seen[[fsm, u, v, qu]] = true
                 end
               end
             end
+            seen[[fsm, u, v]] = true
           end
         end
       end
