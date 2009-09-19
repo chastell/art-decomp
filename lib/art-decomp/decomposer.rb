c4982106d825318006600603e36c1b5dffa1fb5b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-09-19 21:33:32 +0200

    Decomposer#decompositions: turn seen into a proper Set

--- lib/art-decomp/decomposer.rb
+++ lib/art-decomp/decomposer.rb
@@ -8,28 +8,28 @@ module ArtDecomp class Decomposer
   end
 
   def decompositions opts = {}
-    seen = {}
+    seen = Set[]
     Enumerator.new do |yielder|
       @uv_gens.each do |uv_gen|
         uv_gen.uv_pairs.each do |fsm, u, v|
-          unless seen[[fsm, u, v]]
+          unless seen.include? [fsm, u, v]
             @qu_gens.each do |qu_gen|
               qu_gen.blankets(fsm, u, v).each do |qu|
-                unless seen[[fsm, u, v, qu]]
+                unless seen.include? [fsm, u, v, qu]
                   @qv_gens.each do |qv_gen|
                     qv_gen.blankets(fsm, u, v, qu).each do |qv, g|
-                      unless seen[[fsm, u, v, qu, qv, g]]
+                      unless seen.include? [fsm, u, v, qu, qv, g]
                         dec = Decomposition.new fsm, u, v, qu, qv, g, :uv_gen => uv_gen.class, :qu_gen => qu_gen.class, :qv_gen => qv_gen.class
                         yielder.yield dec if dec.sensible? @archs
-                        seen[[fsm, u, v, qu, qv, g]] = true unless opts[:keep_seen]
+                        seen << [fsm, u, v, qu, qv, g] unless opts[:keep_seen]
                       end
                     end
                   end
-                  seen[[fsm, u, v, qu]] = true unless opts[:keep_seen]
+                  seen << [fsm, u, v, qu] unless opts[:keep_seen]
                 end
               end
             end
-            seen[[fsm, u, v]] = true unless opts[:keep_seen]
+            seen << [fsm, u, v] unless opts[:keep_seen]
           end
         end
       end
