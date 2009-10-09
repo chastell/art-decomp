0decb4d3ce73cec4b4d302a1ceb3656c1d7eac73
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-10-09 23:48:09 +0200

    make the String spec pass in Ruby 1.8

--- lib/string.rb
+++ lib/string.rb
@@ -1,7 +1,7 @@
 class String
 
   def dc_expand columns = (0...size).to_a
-    return [self] unless include? ArtDecomp::DontCare
+    return [self] unless include? ArtDecomp::DontCare.to_s
     offsets = columns.dup
     i = index ArtDecomp::DontCare.to_s, offsets.shift until columns.include? i or offsets.empty?
     return [self] unless columns.include? i
