7c9aa368b6d3356bee07e4b4e062c39c2d00b413
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-11-22 03:03:26 +0100

    make String#dc_expand specs use Sets

--- spec/core/string_spec.rb
+++ spec/core/string_spec.rb
@@ -10,8 +10,8 @@ describe String do
 
   it 'the expansion should be limitable to just a given set of columns' do
     '---'.dc_expand.should == ['000', '001', '010', '011', '100', '101', '110', '111']
-    '---'.dc_expand([0,2]).should == ['0-0', '0-1', '1-0', '1-1']
-    '---'.dc_expand([1]).should   == ['-0-', '-1-']
+    '---'.dc_expand(Set[0,2]).should == ['0-0', '0-1', '1-0', '1-1']
+    '---'.dc_expand(Set[1]).should   == ['-0-', '-1-']
   end
 
 end
