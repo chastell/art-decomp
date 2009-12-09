4ca6db78a61457089c5d72f9663da506c30068f8
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-12-09 10:44:34 +0100

    factor out UVRelevanceGenerator

--- lib/art-decomp/uv_generator/general_relevance.rb
+++ lib/art-decomp/uv_generator/general_relevance.rb
@@ -1,20 +1,7 @@
-module ArtDecomp class UVGenerator::GeneralRelevance
+module ArtDecomp class UVGenerator::GeneralRelevance < UVRelevanceGenerator
 
   def uv_pairs fsm, archs
-    @fsm         = fsm
-    @relevance   = fsm.general_relevance.reverse
-    @max_v_sizes = archs.map(&:pins).to_set
-    @cache = Set[]
-    Enumerator.new do |yielder|
-      (0...2**@relevance.size).each do |vector|
-        bits = vector.bits
-        next unless @max_v_sizes.include? bits.size
-        v = @relevance.values_at(*bits).compact.to_set
-        u = (@relevance - v.to_a).compact.to_set
-        yielder.yield @fsm.expand_x(v), u, v unless @cache.include? v
-        @cache << v
-      end
-    end
+    super fsm, archs, :general_relevance
   end
 
 end end
---
+++ lib/art-decomp/uv_relevance_generator.rb
@@ -0,0 +1,20 @@
+module ArtDecomp class UVRelevanceGenerator
+
+  def uv_pairs fsm, archs, method
+    @fsm         = fsm
+    @relevance   = fsm.send(method).reverse
+    @max_v_sizes = archs.map(&:pins).to_set
+    @cache = Set[]
+    Enumerator.new do |yielder|
+      (0...2**@relevance.size).each do |vector|
+        bits = vector.bits
+        next unless @max_v_sizes.include? bits.size
+        v = @relevance.values_at(*bits).compact.to_set
+        u = (@relevance - v.to_a).compact.to_set
+        yielder.yield @fsm.expand_x(v), u, v unless @cache.include? v
+        @cache << v
+      end
+    end
+  end
+
+end end
