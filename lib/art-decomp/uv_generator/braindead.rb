920335c8a47d9fa41793e0e499bb477552ce4088
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-12-09 11:12:13 +0100

    simplify UVGenerator::Braindead#uv_pairs

--- lib/art-decomp/uv_generator/braindead.rb
+++ lib/art-decomp/uv_generator/braindead.rb
@@ -1,17 +1,16 @@
 module ArtDecomp class UVGenerator::Braindead
 
   def uv_pairs fsm, archs
-    @fsm         = fsm
-    @input_count = fsm.input_count
-    @max_v_size  = archs.map(&:pins).max
+    input_count = fsm.input_count
+    inputs      = (0...input_count).to_a
+    max_v_size  = archs.map(&:pins).max
     Enumerator.new do |yielder|
-      inputs = (0...@input_count).to_a
-      (0...2**@input_count).each do |vector|
+      (0...2**input_count).each do |vector|
         u, v = Set[], Set[]
-        @input_count.times do |bit|
+        input_count.times do |bit|
           (vector[bit].zero? ? u : v) << inputs[bit]
         end
-        yielder.yield @fsm.expand_x(v), u, v if v.size <= @max_v_size
+        yielder.yield fsm.expand_x(v), u, v if v.size <= max_v_size
       end
     end
   end
