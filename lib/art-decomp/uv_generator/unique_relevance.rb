5f1017656921021ad3c397026730e496777cbcd4
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-12-09 09:54:03 +0100

    add UVGenerator::UniqueRelevance

---
+++ lib/art-decomp/uv_generator/unique_relevance.rb
@@ -0,0 +1,23 @@
+module ArtDecomp class UVGenerator::UniqueRelevance
+
+  def initialize fsm, archs
+    @fsm         = fsm
+    @relevance   = fsm.unique_relevance.reverse
+    @max_v_sizes = archs.map(&:pins).to_set
+  end
+
+  def uv_pairs
+    @cache = Set[]
+    Enumerator.new do |yielder|
+      (0...2**@relevance.size).each do |vector|
+        bits = vector.bits
+        next unless @max_v_sizes.include? bits.size
+        v = @relevance.values_at(*bits).compact.to_set
+        u = (@relevance - v.to_a).compact.to_set
+        yielder.yield @fsm.expand_x(v), u, v unless @cache.include? v
+        @cache << v
+      end
+    end
+  end
+
+end end
---
+++ spec/art-decomp/uv_generator/unique_relevance_spec.rb
@@ -0,0 +1,61 @@
+module ArtDecomp describe UVGenerator::UniqueRelevance do
+
+  context 'given a certain FSM and a Set of Archs' do
+
+    it 'should yield U and V combinations in a relevance-based order' do
+      fsm = mock FSM, :input_count => 6, :unique_relevance => [0, 1, 2, nil, nil, nil, 3, 4, 5]
+      fsm.stub!(:expand_x).and_return fsm
+      archs = Set[Arch[3,1]]
+      uv_gen = UVGenerator::UniqueRelevance.new fsm, archs
+      uvs = uv_gen.uv_pairs.to_a
+      uvs.size.should == 42
+      uvs[0].should   == [fsm, Set[0,1,2], Set[3,4,5]]
+      uvs[1].should   == [fsm, Set[0,1,2,3], Set[4,5]]
+      uvs[2].should   == [fsm, Set[0,1,2,4], Set[3,5]]
+      uvs[3].should   == [fsm, Set[0,1,2,5], Set[3,4]]
+      uvs[4].should   == [fsm, Set[0,1,2,3,4], Set[5]]
+      uvs[5].should   == [fsm, Set[0,1,2,3,5], Set[4]]
+      uvs[6].should   == [fsm, Set[0,1,2,4,5], Set[3]]
+      uvs[7].should   == [fsm, Set[0,1,2,3,4,5], Set[]]
+      uvs[8].should   == [fsm, Set[0,1,3], Set[2,4,5]]
+      uvs.last.should == [fsm, Set[3,4,5], Set[0,1,2]]
+    end
+
+    it 'should consider all architecture widths when generating the UV sets' do
+      fsm = mock FSM, :input_count => 6, :unique_relevance => [0, 1, 2, nil, nil, nil, 3, 4, 5]
+      fsm.stub!(:expand_x).and_return fsm
+      archs = Set[Arch[3,1], Arch[2,1]]
+      uv_gen = UVGenerator::UniqueRelevance.new fsm, archs
+      uvs = uv_gen.uv_pairs.to_a
+      uvs.size.should == 42
+      uvs[0].should   == [fsm, Set[0,1,2,3], Set[4,5]]
+      uvs[1].should   == [fsm, Set[0,1,2,4], Set[3,5]]
+      uvs[2].should   == [fsm, Set[0,1,2,5], Set[3,4]]
+      uvs[3].should   == [fsm, Set[0,1,2], Set[3,4,5]]
+      uvs[4].should   == [fsm, Set[0,1,2,3,4], Set[5]]
+      uvs[5].should   == [fsm, Set[0,1,2,3,5], Set[4]]
+      uvs[6].should   == [fsm, Set[0,1,2,4,5], Set[3]]
+      uvs[7].should   == [fsm, Set[0,1,2,3,4,5], Set[]]
+      uvs[8].should   == [fsm, Set[0,1,3,4], Set[2,5]]
+      uvs[9].should   == [fsm, Set[0,1,3,5], Set[2,4]]
+      uvs[10].should  == [fsm, Set[0,1,3], Set[2,4,5]]
+      uvs.last.should == [fsm, Set[3,4,5], Set[0,1,2]]
+    end
+
+    it 'should yield V-expanded FSMs' do
+      fsm = mock FSM, :input_count => 2, :unique_relevance => [1, 0, nil, nil, nil]
+      fsm0, fsm1, fsm2, fsm3 = mock(FSM), mock(FSM), mock(FSM), mock(FSM)
+      fsm.should_receive(:expand_x).exactly(4).times.and_return(fsm0, fsm1, fsm2, fsm3)
+      archs = Set[Arch[3,1]]
+      uv_gen = UVGenerator::UniqueRelevance.new fsm, archs
+      uv_gen.uv_pairs.to_a.should == [
+        [fsm0, Set[0,1], Set[]],
+        [fsm1, Set[1],   Set[0]],
+        [fsm2, Set[0],   Set[1]],
+        [fsm3, Set[],    Set[0,1]],
+      ]
+    end
+
+  end
+
+end end
