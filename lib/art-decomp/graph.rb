4af129cdd638949c22e302a0a3369334323a7a50
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2009-12-29 15:51:08 +0100

    simplify Graph#initialize

--- lib/art-decomp/graph.rb
+++ lib/art-decomp/graph.rb
@@ -3,10 +3,9 @@ module ArtDecomp class Graph
   def initialize blanket, seps
     vertices = blanket.ints.dup
     vertices.delete_if { |this| vertices.any? { |other| other != this and other & this == this } }
+    @edges = Hash[vertices.map { |vertex| [vertex, Set[]] }]
     relevant = Hash[vertices.map { |v| [v, seps.select { |s| v&s != 0 and v&s != s }.to_set] }]
-    @edges = Hash.new { |h, k| h[k] = Set[] }
-    vertices.each { |v| @edges[v] = Set[] }
-    vertices.pairs.select do |a, b|
+    vertices.pairs.each do |a, b|
       if (relevant[a] & relevant[b]).any? { |s| a&s != b&s }
         @edges[a] << b
         @edges[b] << a
