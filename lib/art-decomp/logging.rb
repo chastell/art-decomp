8175d9ee12eab852707add777734486a14635eda
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-02-16 23:44:31 +0100

    Logging: use singleton class’s instance variables

--- lib/art-decomp/logging.rb
+++ lib/art-decomp/logging.rb
@@ -8,57 +8,57 @@ module ArtDecomp class Logging
 class << self
 
   def level
-    @@log.level
+    @log.level
   end
 
   def level= level
-    @@log.level = level
+    @log.level = level
   end
 
   def log= log
-    @@start = Time.now
-    @@log = Logger.new log
-    @@log.level = Logger::INFO
-    @@log.formatter = proc { |sev, date, name, msg| "#{(Time.now - @@start).ceil.to_s.rjust 6}s #{msg}\n" }
+    @start = Time.now
+    @log = Logger.new log
+    @log.level = Logger::INFO
+    @log.formatter = proc { |sev, date, name, msg| "#{(Time.now - @start).ceil.to_s.rjust 6}s #{msg}\n" }
 
     Executable.class_eval { include RCapture::Interceptable }
 
     Executable.capture_post :methods => :run do |point|
-      secs = (Time.now - @@start).to_i
-      @@log.info "#{point.sender.best ? "final best decomposition: #{point.sender.best} cells" : 'no final decomposition'}; done in #{secs}s (#{secs / 60 / 60}h #{secs / 60 % 60}m #{secs % 60}s)"
+      secs = (Time.now - @start).to_i
+      @log.info "#{point.sender.best ? "final best decomposition: #{point.sender.best} cells" : 'no final decomposition'}; done in #{secs}s (#{secs / 60 / 60}h #{secs / 60 % 60}m #{secs % 60}s)"
     end
 
     Executable.capture_pre :methods => :decompositions do |point|
       path     = point.args[2][point.sender.dir.size+1..-1]
       archs    = point.sender.archs.map(&:to_s).sort.reverse.join '+'
-      @@log.info "FSM #{point.args[0].stats} → #{archs} (#{path}) with #{point.sender.gens} – #{point.sender.best ? "best so far: #{point.sender.best} cells" : 'no decomposition so far'}"
+      @log.info "FSM #{point.args[0].stats} → #{archs} (#{path}) with #{point.sender.gens} – #{point.sender.best ? "best so far: #{point.sender.best} cells" : 'no decomposition so far'}"
     end
 
     UVGenerator.constants.map { |c| eval("UVGenerator::#{c}") }.each do |uv_gen|
       uv_gen.class_eval { include RCapture::Interceptable }
       uv_gen.capture_pre :methods => :uv_pairs do |point|
-        @@log.info "  UV with #{point.sender.class.to_s.split('::').last}"
+        @log.info "  UV with #{point.sender.class.to_s.split('::').last}"
       end
     end
 
     QuGenerator.constants.map { |c| eval("QuGenerator::#{c}") }.each do |qu_gen|
       qu_gen.class_eval { include RCapture::Interceptable }
       qu_gen.capture_pre :methods => :blankets do |point|
-        @@log.info "    U = #{point.args[1].sort.inspect}, V = #{point.args[2].sort.inspect}, Qu with #{point.sender.class.to_s.split('::').last}"
+        @log.info "    U = #{point.args[1].sort.inspect}, V = #{point.args[2].sort.inspect}, Qu with #{point.sender.class.to_s.split('::').last}"
       end
     end
 
     QvGenerator.constants.map { |c| eval("QvGenerator::#{c}") }.each do |qv_gen|
       qv_gen.class_eval { include RCapture::Interceptable }
       qv_gen.capture_pre :methods => :blankets do |point|
-        @@log.debug "      |Qu| = #{point.args[3].size}, Qv+G with #{point.sender.class.to_s.split('::').last}"
+        @log.debug "      |Qu| = #{point.args[3].size}, Qv+G with #{point.sender.class.to_s.split('::').last}"
       end
     end
   end
 
   def off
-    # FIXME: if methods can be uncaptured, do that and close @@log
-    @@log = Logger.new '/dev/null'
+    # FIXME: if methods can be uncaptured, do that and close @log
+    @log = Logger.new '/dev/null'
   end
 
 end
