dc179d2f897ab95acd758ffb97123ea7f68b3a64
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-11-27 23:13:07 +0100

    respec Integer#bits

--- spec/core/integer_spec.rb
+++ spec/core/integer_spec.rb
@@ -1,11 +1,15 @@
 describe Integer do
 
-  it 'should return set bits' do
-    0.bits.should        == []
-    0b111.bits.should    == [0,1,2]
-    0b1000.bits.should   == [3]
-    0b101010.bits.should == [1,3,5]
-    (2**69).bits.should  == [69]
+  context '#bits' do
+
+    it 'returns set bit positions' do
+      0.bits.should        == []
+      0b111.bits.should    == [0,1,2]
+      0b1000.bits.should   == [3]
+      0b101010.bits.should == [1,3,5]
+      (2**69).bits.should  == [69]
+    end
+
   end
 
   it 'should return ceil of base-2 logarithm' do
