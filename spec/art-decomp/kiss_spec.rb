15fa0b895519dadddc88b7ebc9575750a0ab2829
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-11-27 23:56:24 +0100

    respec KISS#formatted

--- spec/art-decomp/kiss_spec.rb
+++ spec/art-decomp/kiss_spec.rb
@@ -1,30 +1,34 @@
 module ArtDecomp describe KISS do
 
-  it 'should sort entries' do
-    kiss = KISS.new ['1 1', '0 0']
-    kiss.formatted.should == "0 0\n1 1\n"
-  end
+  context '#formatted' do
 
-  it 'should drop non-unique entries' do
-    kiss = KISS.new ['0 0', '0 0']
-    kiss.formatted.should == "0 0\n"
-  end
+    it 'sorts entries' do
+      kiss = KISS.new ['1 1', '0 0']
+      kiss.formatted.should == "0 0\n1 1\n"
+    end
 
-  it 'should drop overlapping entries' do
-    kiss = KISS.new ['0-0 0', '010 0', '1-1 1', '--1 1']
-    kiss.formatted.should == "--1 1\n0-0 0\n"
-  end
+    it 'drops non-unique entries' do
+      kiss = KISS.new ['0 0', '0 0']
+      kiss.formatted.should == "0 0\n"
+    end
 
-  it 'should preserve overlapping entries which differ on subsequent column groups' do
-    kiss = KISS.new ['-- 1 0 1', '10 0 0 1']
-    kiss.formatted.should == "-- 1 0 1\n10 0 0 1\n"
-  end
+    it 'drops overlapping entries' do
+      kiss = KISS.new ['0-0 0', '010 0', '1-1 1', '--1 1']
+      kiss.formatted.should == "--1 1\n0-0 0\n"
+    end
 
-  it 'should combine matching entries' do
-    kiss = KISS.new ['00 0 1 1', '01 0 1 1', '10 1 0 0', '11 1 0 0']
-    kiss.formatted.should == "0- 0 1 1\n1- 1 0 0\n"
-    kiss = KISS.new ['00 0', '11 0', '10 0', '01 0']
-    kiss.formatted.should == "-- 0\n"
+    it 'preserves overlapping entries differing on subsequent column groups' do
+      kiss = KISS.new ['-- 1 0 1', '10 0 0 1']
+      kiss.formatted.should == "-- 1 0 1\n10 0 0 1\n"
+    end
+
+    it 'combines matching entries' do
+      kiss = KISS.new ['00 0 1 1', '01 0 1 1', '10 1 0 0', '11 1 0 0']
+      kiss.formatted.should == "0- 0 1 1\n1- 1 0 0\n"
+      kiss = KISS.new ['00 0', '11 0', '10 0', '01 0']
+      kiss.formatted.should == "-- 0\n"
+    end
+
   end
 
 end end
