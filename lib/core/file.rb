65704e58a59bff15cb98d5f82c69253bcf133b30
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-11-27 23:10:28 +0100

    drop File.write_data

--- lib/core/file.rb
+++ lib/core/file.rb
@@ -1,11 +1,7 @@
 class File
 
   def self.dump_object object, path
-    write_data Marshal.dump(object), path
-  end
-
-  def self.write_data data, path
-    self.open(path, 'w') { |f| f << data }
+    open(path, 'w') { |file| file.write Marshal.dump object }
   end
 
 end
--- spec/core/file_spec.rb
+++ spec/core/file_spec.rb
@@ -10,10 +10,4 @@ describe File do
 
   end
 
-  it 'should be able to dump arbitrary data to a given file' do
-    file = Tempfile.new rand.to_s
-    File.write_data '42', file.path
-    File.read(file.path).should == '42'
-  end
-
 end
