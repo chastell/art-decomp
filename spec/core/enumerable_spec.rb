f6a80cd181f4225c1cd29182fe06f7cec39aea5a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-11-27 22:39:47 +0100

    respec Enumerable#pairs

--- spec/core/enumerable_spec.rb
+++ spec/core/enumerable_spec.rb
@@ -1,14 +1,19 @@
 describe Enumerable do
 
-  it 'should provide an Enumerator for consecutive element pairs' do
-    set = Set[1,2,3,4]
-    set.pairs.to_a.should == [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
-  end
+  context '#pairs' do
 
-  it 'should fall back to #combination for the pair selection if possible' do
-    array = [1,2,3,4]
-    array.should_receive(:combination).with 2
-    array.pairs
+    it 'provides an Enumerator for consecutive element pairs' do
+      set = Set[1,2,3,4]
+      set.pairs.is_a? Enumerator
+      set.pairs.to_a.should == [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
+    end
+
+    it 'falls back to #combination for the pair selection if possible' do
+      array = [1,2,3,4]
+      array.should_receive(:combination).with 2
+      array.pairs
+    end
+
   end
 
 end
