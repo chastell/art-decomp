95fcc267eab90adab891aa17ca9c3d5dc38ceace
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-11-27 23:51:14 +0100

    respec VHDL#vhdl

--- spec/art-decomp/vhdl_spec.rb
+++ spec/art-decomp/vhdl_spec.rb
@@ -1,19 +1,17 @@
-# encoding: UTF-8
-
 module ArtDecomp describe VHDL do
 
-  context 'generated from an FSM' do
+  context '#vhdl' do
 
-    it 'should properly generates the VHDL representation' do
+    it 'generates the VHDL representation of the underlying FSM' do
       VHDL.new(FSM.from_kiss 'spec/fixtures/fsm').vhdl('fsm').should     == File.read('spec/fixtures/fsm.vhd')
       VHDL.new(FSM.from_kiss 'spec/fixtures/mark1').vhdl('mark1').should == File.read('spec/fixtures/mark1.vhd')
     end
 
-    it 'should generate encoded VHDL if the FSM is encoded' do
+    it 'generates encoded VHDL if the FSM is encoded' do
       VHDL.new(FSM.from_kiss 'spec/fixtures/mark1.hot').vhdl('mark1_hot').should == File.read('spec/fixtures/mark1_hot.vhd')
     end
 
-    it 'should rename digit-only states' do
+    it 'renames digit-only states' do
       VHDL.new(FSM.from_kiss 'spec/fixtures/ex4.hot').vhdl('ex4_hot').should == File.read('spec/fixtures/ex4_hot.vhd')
     end
 
