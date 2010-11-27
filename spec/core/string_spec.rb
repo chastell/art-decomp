79e3f1758694e382ea9c007368da83aedd281c76
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-11-27 23:22:39 +0100

    respec String#dc_expand

--- spec/core/string_spec.rb
+++ spec/core/string_spec.rb
@@ -2,16 +2,20 @@
 
 describe String do
 
-  it 'should provide an expansion of don’t-cares when representing boolean inputs' do
-    '0'.dc_expand.should   == ['0']
-    '-'.dc_expand.should   == ['0', '1']
-    '-1-'.dc_expand.should == ['010', '011', '110', '111']
-  end
+  context '#dc_expand' do
 
-  it 'the expansion should be limitable to just a given set of columns' do
-    '---'.dc_expand.should == ['000', '001', '010', '011', '100', '101', '110', '111']
-    '---'.dc_expand(Set[0,2]).should == ['0-0', '0-1', '1-0', '1-1']
-    '---'.dc_expand(Set[1]).should   == ['-0-', '-1-']
+    it 'expands don’t-cares when representing boolean inputs' do
+      '0'.dc_expand.should   == ['0']
+      '-'.dc_expand.should   == ['0', '1']
+      '-1-'.dc_expand.should == ['010', '011', '110', '111']
+    end
+
+    it 'the expansion is limitable to the given set of columns' do
+      '---'.dc_expand.should           == ['000', '001', '010', '011', '100', '101', '110', '111']
+      '---'.dc_expand(Set[0,2]).should == ['0-0', '0-1', '1-0', '1-1']
+      '---'.dc_expand(Set[1]).should   == ['-0-', '-1-']
+    end
+
   end
 
 end
