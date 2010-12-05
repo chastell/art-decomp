dd3eaafddef69d1a47dbe2f374686e09a4376cb1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-12-05 21:07:25 +0100

    respec UVGenerator::GeneralRelevance

--- spec/art-decomp/uv_generator/general_relevance_spec.rb
+++ spec/art-decomp/uv_generator/general_relevance_spec.rb
@@ -2,10 +2,14 @@
 
 module ArtDecomp describe UVGenerator::GeneralRelevance do
 
-  it 'should call UVRelevanceGenerator’s uv_pairs method with proper args' do
-    fsm = mock FSM
-    fsm.should_receive(:general_relevance).and_return []
-    UVGenerator::GeneralRelevance.new.uv_pairs fsm, Set[Arch[5,1]]
+  context '#uv_pairs' do
+
+    it 'calls UVRelevanceGenerator’s uv_pairs method with proper args' do
+      fsm = mock FSM
+      fsm.should_receive(:general_relevance).and_return []
+      UVGenerator::GeneralRelevance.new.uv_pairs fsm, Set[Arch[5,1]]
+    end
+
   end
 
 end end
