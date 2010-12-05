152202976512a89769a486ac936a6bfa2bce325f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-12-05 21:10:29 +0100

    respec QuGenerator::BlockTable

--- spec/art-decomp/qu_generator/block_table_spec.rb
+++ spec/art-decomp/qu_generator/block_table_spec.rb
@@ -2,30 +2,30 @@
 
 module ArtDecomp describe QuGenerator::BlockTable do
 
-  context 'when generating Qu blankets' do
+  context '#blankets' do
 
     def qus
-      fsm   = mock FSM, :beta_f => @beta_f, :beta_q => @beta_q, :beta_x => @beta_x
+      fsm   = mock FSM, beta_f: @beta_f, beta_q: @beta_q, beta_x: @beta_x
       archs = Set[Arch[3,1]]
       bt    = QuGenerator::BlockTable.new
       bt.blankets(fsm, Set[0], Set[1]).to_a
     end
 
-    it 'should fold Q blocks that match in the first go' do
+    it 'folds Q blocks that match in the first go' do
       @beta_f = Blanket[]
       @beta_q = Blanket[B[1,2], B[2], B[3]]
       @beta_x = Blanket[B[1], B[2], B[3]]
       qus.first.should == Blanket[B[1,2,3], B[2]]
     end
 
-    it 'shouldn’t fold Q blocks if they don’t match' do
+    it 'doesn’t fold Q blocks if they don’t match' do
       @beta_f = Blanket[]
       @beta_q = Blanket[B[1,2], B[2,3]]
       @beta_x = Blanket[B[1], B[2], B[3]]
       qus.first.should == Blanket[B[1,2], B[2,3]]
     end
 
-    it 'should first try to fold matching blocks and then fold by r-admissibility' do
+    it 'first tries to fold matching blocks and then folds by r-admissibility' do
       @beta_f = mock Blanket, :seps => Set[Sep[1,2], Sep[2,3]]
       @beta_q = Blanket[B[1], B[2], B[3]]
       @beta_x = Blanket[B[1,2,3], B[1,3]]
