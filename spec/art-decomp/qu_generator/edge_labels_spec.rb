4756f91b10eab2703f01135e1af497a07be44c45
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-12-07 06:47:06 +0100

    switch QuGenerator::EdgeLabels spec to Ruby 1.9 Hash syntax

--- spec/art-decomp/qu_generator/edge_labels_spec.rb
+++ spec/art-decomp/qu_generator/edge_labels_spec.rb
@@ -10,7 +10,7 @@ module ArtDecomp describe QuGenerator::EdgeLabels do
     end
 
     it 'fist merges until its graph is complete' do
-      @beta_f = mock Blanket, :seps => Set[Sep[1,2], Sep[2,3]]
+      @beta_f = mock Blanket, seps: Set[Sep[1,2], Sep[2,3]]
       @beta_q = Blanket[B[1], B[2], B[3]]
       @beta_x = Blanket[]
       qus.first.should == Blanket[B[1,3], B[2]]
