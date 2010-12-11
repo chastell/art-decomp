91802d090f33e1f4eb237fd0b0f61899292cbd15
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-12-11 10:30:23 +0100

    switch QuGenerator::BlockTable spec to Ruby 1.9 Hash syntax

--- spec/art-decomp/qu_generator/block_table_spec.rb
+++ spec/art-decomp/qu_generator/block_table_spec.rb
@@ -26,7 +26,7 @@ module ArtDecomp describe QuGenerator::BlockTable do
     end
 
     it 'first tries to fold matching blocks and then folds by r-admissibility' do
-      @beta_f = mock Blanket, :seps => Set[Sep[1,2], Sep[2,3]]
+      @beta_f = mock Blanket, seps: Set[Sep[1,2], Sep[2,3]]
       @beta_q = Blanket[B[1], B[2], B[3]]
       @beta_x = Blanket[B[1,2,3], B[1,3]]
       qus.should == [Blanket[B[1], B[2], B[3]], Blanket[B[1,3], B[2]], Blanket[B[1,2,3]]]
