2a26d869339d95f9c80dd2d57eb9c0d7a09c9f33
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2010-12-11 14:54:13 +0100

    typo fix

--- spec/core/file_spec.rb
+++ spec/core/file_spec.rb
@@ -2,7 +2,7 @@ describe File do
 
   describe '.dump_object' do
 
-    it 'marshals the given object to a file at the given file' do
+    it 'marshals the given object to a file at the given path' do
       file = Tempfile.new 'File.dump_object'
       File.dump_object [:answer, 42, 'Deep Thought'], file.path
       Marshal.load(File.read(file.path)).should == [:answer, 42, 'Deep Thought']
