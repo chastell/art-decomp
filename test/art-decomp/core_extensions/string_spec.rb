e651207aee01f29f0da9ccd57f8f4cdd010156e2
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-10-22 16:02:03 +0200

    port CoreExtensions::String spec to minitest/spec

---
+++ test/art-decomp/core_extensions/string_spec.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+
+require_relative '../../spec_helper'
+
+module ArtDecomp describe CoreExtensions::String do
+
+  describe '#dc_expand' do
+
+    it 'expands don’t-cares when representing boolean inputs' do
+      '0'.extend(CoreExtensions::String).dc_expand.must_equal   ['0']
+      '-'.extend(CoreExtensions::String).dc_expand.must_equal   ['0', '1']
+      '-1-'.extend(CoreExtensions::String).dc_expand.must_equal ['010', '011', '110', '111']
+    end
+
+    it 'the expansion is limitable to the given set of columns' do
+      '---'.extend(CoreExtensions::String).dc_expand.must_equal           ['000', '001', '010', '011', '100', '101', '110', '111']
+      '---'.extend(CoreExtensions::String).dc_expand(Set[0,2]).must_equal ['0-0', '0-1', '1-0', '1-1']
+      '---'.extend(CoreExtensions::String).dc_expand(Set[1]).must_equal   ['-0-', '-1-']
+    end
+
+  end
+
+end end
