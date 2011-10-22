93a388b758f02366a5c79ae94de562e97bd85297
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-10-22 15:49:42 +0200

    port Sep spec to minitest/spec

---
+++ test/art-decomp/sep_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../spec_helper'
+
+module ArtDecomp describe Sep do
+
+  describe '.[]' do
+
+    it 'constructs an Integer with set bits representing the separation' do
+      Sep[1,2].must_equal  0b110
+      Sep[0,69].must_equal 2**69 + 0b1
+    end
+
+  end
+
+  describe '#hash' do
+
+    it 'hashes uniformly' do
+      Sep[1,2].hash.must_equal  0b110.hash
+      Sep[0,69].hash.must_equal (2**69 + 0b1).hash
+    end
+
+  end
+
+end end
