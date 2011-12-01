1a80b0673f6d5faaddc84b51f5db0f6675e22224
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-01 21:50:13 +0100

    cleanup CoreExtensions::Integer and spec

--- lib/art-decomp/core_extensions/integer.rb
+++ lib/art-decomp/core_extensions/integer.rb
@@ -1,6 +1,4 @@
 module ArtDecomp module CoreExtensions::Integer
-
-  # FIXME: consider right-shifting self and counting ones
   def bits
     (0...to_s(2).size).select { |bit| self[bit] == 1 }
   end
@@ -9,5 +7,4 @@ module ArtDecomp module CoreExtensions::Integer
     return 0 if self == 0 or self == 1
     (self - 1).to_s(2).size
   end
-
 end end
--- spec/art-decomp/core_extensions/integer_spec.rb
+++ spec/art-decomp/core_extensions/integer_spec.rb
@@ -1,9 +1,7 @@
 require_relative '../../spec_helper'
 
 module ArtDecomp describe CoreExtensions::Integer do
-
   describe '#bits' do
-
     it 'returns set bit positions' do
       0.bits.must_equal        []
       0b111.bits.must_equal    [0,1,2]
@@ -11,16 +9,12 @@ module ArtDecomp describe CoreExtensions::Integer do
       0b101010.bits.must_equal [1,3,5]
       (2**69).bits.must_equal  [69]
     end
-
   end
 
   describe '#log2_ceil' do
-
     it 'returns ceil of base-two logarithm from self' do
       (0..9).map(&:log2_ceil).must_equal [0,0,1,2,2,3,3,3,3,4]
       (2**69).log2_ceil.must_equal       69
     end
-
   end
-
 end end
