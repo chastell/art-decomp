842a62ebec64b3a1216794f10a0a9fc5b20162c4
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-01 23:13:42 +0100

    cleanup B and spec

--- spec/art-decomp/b_spec.rb
+++ spec/art-decomp/b_spec.rb
@@ -1,9 +1,7 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe B do
-
   describe '.[]' do
-
     it 'constructs an Integer with the given bits set' do
       B[].must_equal     0b0
       B[0].must_equal    0b1
@@ -12,11 +10,9 @@ module ArtDecomp describe B do
       B[40].must_equal   2**40
       B[1,69].must_equal 2**69 + 0b10
     end
-
   end
 
   describe '#hash' do
-
     it 'hashes uniformly' do
       B[].hash.must_equal     0b0.hash
       B[0].hash.must_equal    0b1.hash
@@ -25,7 +21,5 @@ module ArtDecomp describe B do
       B[40].hash.must_equal   (2**40).hash
       B[1,69].hash.must_equal (2**69 + 0b10).hash
     end
-
   end
-
 end end
