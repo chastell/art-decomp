f021a1319cf01a79232399e34010b2daa4a826b1
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-30 17:44:52 +0100

    cleanup UVGenerator::GeneralRelevance and spec

--- lib/art-decomp/uv_generator/general_relevance.rb
+++ lib/art-decomp/uv_generator/general_relevance.rb
@@ -1,7 +1,5 @@
 module ArtDecomp class UVGenerator::GeneralRelevance < UVRelevanceGenerator
-
   def uv_pairs fsm, archs
     super fsm, archs, :general_relevance
   end
-
 end end
--- spec/art-decomp/uv_generator/general_relevance_spec.rb
+++ spec/art-decomp/uv_generator/general_relevance_spec.rb
@@ -3,16 +3,11 @@
 require_relative '../../spec_helper'
 
 module ArtDecomp describe UVGenerator::GeneralRelevance do
-
   describe '#uv_pairs' do
-
     it 'calls UVRelevanceGenerator’s uv_pairs method with proper args' do
-      fsm = MiniTest::Mock.new
-      fsm.expect :send, [], [:general_relevance]
+      fsm = MiniTest::Mock.new.expect :send, [], [:general_relevance]
       UVGenerator::GeneralRelevance.new.uv_pairs fsm, Set[Arch[5,1]]
       fsm.verify
     end
-
   end
-
 end end
