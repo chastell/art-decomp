19f88f670a7a2fca52402e8ed261295780bb5a5d
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-12-31 14:46:34 +0100

    cleanup QvGenerator::GraphMerging and spec

--- lib/art-decomp/qv_generator/graph_merging.rb
+++ lib/art-decomp/qv_generator/graph_merging.rb
@@ -1,5 +1,4 @@
 module ArtDecomp class QvGenerator::GraphMerging
-
   def blankets fsm, u, v, beta_qu
     beta_u   = fsm.beta_x u
     beta_v   = fsm.beta_x v
@@ -8,12 +7,11 @@ module ArtDecomp class QvGenerator::GraphMerging
       loop do
         beta_qv = Blanket.new qv_graph.vertices
         g_graph = Graph.new beta_qv * beta_v, fsm.beta_f.seps - beta_u.seps - beta_qu.seps
-        beta_g = Blanket.new g_graph.merge_until_complete!.vertices
+        beta_g  = Blanket.new g_graph.merge_until_complete!.vertices
         yielder.yield [beta_qv, beta_g]
         break if qv_graph.complete?
         qv_graph.merge_by_vertex_degrees!
       end
     end
   end
-
 end end
