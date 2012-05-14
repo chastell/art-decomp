62057465f7d8c44956fc1611bee2ae016cb810d6
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-05-14 22:18:22 +0200

    Executable: introduce Trollop

--- lib/art-decomp/executable.rb
+++ lib/art-decomp/executable.rb
@@ -1,6 +1,9 @@
+require 'trollop'
+
 module ArtDecomp class Executable
   def initialize args
-    abort 'no FSM given'       if     args.empty?
-    abort 'FSM does not exist' unless File.exists? args.first
+    Trollop.options args
+    Trollop.die 'no FSM given'       if     args.empty?
+    Trollop.die 'FSM does not exist' unless File.exists? args.first
   end
 end end
--- spec/art-decomp/executable_spec.rb
+++ spec/art-decomp/executable_spec.rb
@@ -3,6 +3,13 @@ require_relative '../../lib/art-decomp/executable'
 
 module ArtDecomp describe Executable do
   describe '.new' do
+    it 'gives help when asked' do
+      -> { -> { Executable.new ['--help'] }.must_raise SystemExit }.must_output <<-end.gsub(/^ {8}/, '')
+        Options:
+          --help, -h:   Show this message
+      end
+    end
+
     it 'requires a path to an existing FSM' do
       capture_io { -> { Executable.new []        }.must_raise SystemExit }.last.must_include 'no FSM given'
       capture_io { -> { Executable.new ['bogus'] }.must_raise SystemExit }.last.must_include 'FSM does not exist'
