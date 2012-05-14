2acefcb1b685a069467cd808adacd5d52886c89e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-05-14 21:53:31 +0200

    Executable: abort on bogus FSM paths

--- lib/art-decomp/executable.rb
+++ lib/art-decomp/executable.rb
@@ -1,5 +1,6 @@
 module ArtDecomp class Executable
   def initialize args
-    abort 'no FSM given' if args.empty?
+    abort 'no FSM given'       if     args.empty?
+    abort 'FSM does not exist' unless File.exists? args.first
   end
 end end
--- spec/art-decomp/executable_spec.rb
+++ spec/art-decomp/executable_spec.rb
@@ -4,7 +4,8 @@ require_relative '../../lib/art-decomp/executable'
 module ArtDecomp describe Executable do
   describe '.new' do
     it 'requires a path to an existing FSM' do
-      capture_io { -> { Executable.new [] }.must_raise SystemExit }.last.must_include 'no FSM given'
+      capture_io { -> { Executable.new []        }.must_raise SystemExit }.last.must_include 'no FSM given'
+      capture_io { -> { Executable.new ['bogus'] }.must_raise SystemExit }.last.must_include 'FSM does not exist'
     end
   end
 end end
