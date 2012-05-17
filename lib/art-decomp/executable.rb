5556a7fef67b0afe92937a18ede3e2723242a300
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-05-18 01:26:07 +0200

    Executable: get them archs required

--- lib/art-decomp/executable.rb
+++ lib/art-decomp/executable.rb
@@ -2,8 +2,10 @@ require 'trollop'
 
 module ArtDecomp class Executable
   def initialize args
-    Trollop.options args
-    Trollop.die 'no FSM given'       if     args.empty?
-    Trollop.die 'FSM does not exist' unless File.exists? args.first
+    opts = Trollop.options args
+
+    Trollop.die 'no FSM given'             if     args.empty?
+    Trollop.die 'FSM does not exist'       unless File.exists? args.first
+    Trollop.die 'no architecture(s) given' unless opts[:archs_given]
   end
 end end
--- spec/art-decomp/executable_spec.rb
+++ spec/art-decomp/executable_spec.rb
@@ -2,6 +2,8 @@ require_relative '../spec_helper'
 require_relative '../../lib/art-decomp/executable'
 
 module ArtDecomp describe Executable do
+  let(:fsm_path) { 'spec/fixtures/fsm' }
+
   describe '.new' do
     it 'gives help when asked' do
       -> { -> { Executable.new ['--help'] }.must_raise SystemExit }.must_output <<-end.gsub(/^ {8}/, '')
@@ -13,6 +15,10 @@ module ArtDecomp describe Executable do
     it 'requires a path to an existing FSM' do
       capture_io { -> { Executable.new []        }.must_raise SystemExit }.last.must_include 'no FSM given'
       capture_io { -> { Executable.new ['bogus'] }.must_raise SystemExit }.last.must_include 'FSM does not exist'
+    end
+
+    it 'requires one or more architectures' do
+      capture_io { -> { Executable.new [fsm_path] }.must_raise SystemExit }.last.must_include 'no architecture(s) given'
     end
   end
 end end
