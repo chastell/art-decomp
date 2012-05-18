013c5d9e08070e862476902709eab446147661aa
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-05-19 00:37:08 +0200

    Executable: add dir support

--- lib/art-decomp/executable.rb
+++ lib/art-decomp/executable.rb
@@ -4,10 +4,12 @@ module ArtDecomp class Executable
   def initialize args
     opts = Trollop.options args do
       opt :archs, 'Target architecture(s)', type: :strings
+      opt :dir,   'Results directory',      type: :string
     end
 
     Trollop.die 'no FSM given'             if     args.empty?
     Trollop.die 'FSM does not exist'       unless File.exists? args.first
     Trollop.die 'no architecture(s) given' unless opts[:archs_given]
+    Trollop.die 'no directory given'       unless opts[:dir_given]
   end
 end end
--- spec/art-decomp/executable_spec.rb
+++ spec/art-decomp/executable_spec.rb
@@ -2,14 +2,18 @@ require_relative '../spec_helper'
 require_relative '../../lib/art-decomp/executable'
 
 module ArtDecomp describe Executable do
-  let(:args)     { ['--archs', '5/1', '--', fsm_path] }
+  let(:args)     { ['--archs', '5/1', '--dir', dir_path, '--', fsm_path] }
+  let(:dir_path) { Dir.mktmpdir }
   let(:fsm_path) { 'spec/fixtures/fsm' }
+
+  after { FileUtils.rmtree dir_path }
 
   describe '.new' do
     it 'gives help when asked' do
       -> { -> { Executable.new ['--help'] }.must_raise SystemExit }.must_output <<-end.gsub(/^ {8}/, '')
         Options:
           --archs, -a <s+>:   Target architecture(s)
+             --dir, -d <s>:   Results directory
                 --help, -h:   Show this message
       end
     end
@@ -21,6 +25,10 @@ module ArtDecomp describe Executable do
 
     it 'requires one or more architectures' do
       capture_io { -> { Executable.new [fsm_path] }.must_raise SystemExit }.last.must_include 'no architecture(s) given'
+    end
+
+    it 'requires directory for results' do
+      capture_io { -> { Executable.new ['--archs', '5/1', '--', fsm_path] }.must_raise SystemExit }.last.must_include 'no directory given'
     end
 
     it 'does not raise with minimal arguments' do
