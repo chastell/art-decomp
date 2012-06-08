a1bab98ad5cffd5ba884dff7cf31746dfa997244
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-06-08 02:20:53 +0200

    drop the shortcut

--- lib/art-decomp/uv_generators/relative_relevance.rb
+++ lib/art-decomp/uv_generators/relative_relevance.rb
@@ -1,12 +1,5 @@
 module ArtDecomp module UVGenerators class RelativeRelevance < UVRelevanceGenerator
   def uv_pairs fsm, archs
     super fsm, archs, :relative_relevance
-    # Enumerator.new do |yielder|
-    #   case fsm.input_count
-    #   when 4 then u, v = Set[2], Set[0, 1, 3]
-    #   when 3 then u, v = Set[0], Set[1, 2]
-    #   end
-    #   yielder.yield fsm.expand_x(v), u, v
-    # end
   end
 end end end
