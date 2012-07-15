f07954a57eabade0523c8821f0953b2eb38b94bc
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-07-15 16:12:33 +0200

    introducing DecTreeGenerator#dectrees

--- lib/art-decomp/dec_tree_generator.rb
+++ lib/art-decomp/dec_tree_generator.rb
@@ -1,2 +1,31 @@
 module ArtDecomp class DecTreeGenerator
+  def initialize opts
+    self.archs                = opts.fetch :archs
+    self.fsm                  = opts.fetch :fsm
+    self.gens                 = opts.fetch :gens
+    self.decomposer_generator = opts.fetch :decomposer_generator
+  end
+
+  def dectrees opts = {}
+    this_fsm = opts.fetch :fsm, fsm
+    tree     = opts.fetch :tree, []
+    Enumerator.new do |yielder|
+      decomposer = decomposer_generator.new fsm: this_fsm, archs: archs, gens: gens
+      decomposer.decompositions.each do |dec|
+        tree << dec
+        if dec.final? archs
+          yielder.yield tree.dup
+        else
+          dectrees(fsm: dec.h, tree: tree).each do |subtree|
+            yielder.yield subtree
+          end
+        end
+        tree.pop
+      end
+    end
+  end
+
+  protected
+
+  attr_accessor :archs, :decomposer_generator, :fsm, :gens
 end end
--- spec/art-decomp/dec_tree_generator_spec.rb
+++ spec/art-decomp/dec_tree_generator_spec.rb
@@ -1,4 +1,32 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe DecTreeGenerator do
+  describe '#dectrees' do
+    it 'yields subsequent DecTrees' do
+      archs  = Object.new
+      gens   = Object.new
+      fsm    = Object.new
+      fsm1   = Object.new
+      fsm11  = Object.new
+      dec1   = OpenStruct.new(h: fsm1);  def dec1.final?   *_; false; end
+      dec11  = OpenStruct.new(h: fsm11); def dec11.final?  *_; false; end
+      dec111 = Object.new;               def dec111.final? *_; true;  end
+      dec12  = Object.new;               def dec12.final?  *_; true;  end
+
+      decomposer1   = MiniTest::Mock.new.expect :decompositions, [dec1]
+      decomposer11  = MiniTest::Mock.new.expect :decompositions, [dec11, dec12]
+      decomposer111 = MiniTest::Mock.new.expect :decompositions, [dec111]
+
+      dec_gen = MiniTest::Mock.new
+      dec_gen.expect :new, decomposer1,   [{ archs: archs, fsm: fsm,   gens: gens }]
+      dec_gen.expect :new, decomposer11,  [{ archs: archs, fsm: fsm1,  gens: gens }]
+      dec_gen.expect :new, decomposer111, [{ archs: archs, fsm: fsm11, gens: gens }]
+
+      dt_gen = DecTreeGenerator.new archs: archs, fsm: fsm, gens: gens, decomposer_generator: dec_gen
+      dt_gen.dectrees.to_a.must_equal [
+        [dec1, dec11, dec111],
+        [dec1, dec12],
+      ]
+    end
+  end
 end end
