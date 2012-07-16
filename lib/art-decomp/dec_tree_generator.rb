00d77bc0e6c2abbf8824f56bf68a97db527e6148
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-07-16 22:57:08 +0200

    DecTreeGenerator#dectrees: positional arguments vs SLOC – FIGHT!

--- lib/art-decomp/dec_tree_generator.rb
+++ lib/art-decomp/dec_tree_generator.rb
@@ -6,19 +6,15 @@ module ArtDecomp class DecTreeGenerator
     self.decomposer_generator = opts.fetch :decomposer_generator
   end
 
-  def dectrees opts = {}
-    this_fsm = opts.fetch :fsm, fsm
-    dec_tree = opts.fetch :dec_tree, DecTree.new
+  def dectrees fsm = fsm, dec_tree = DecTree.new
     Enumerator.new do |yielder|
-      decomposer = decomposer_generator.new fsm: this_fsm, archs: archs, gens: gens
+      decomposer = decomposer_generator.new fsm: fsm, archs: archs, gens: gens
       decomposer.decompositions.each do |dec|
         dec_tree << dec
         if dec.final? archs
           yielder.yield dec_tree.dup
         else
-          dectrees(fsm: dec.h, dec_tree: dec_tree).each do |subtree|
-            yielder.yield subtree
-          end
+          dectrees(dec.h, dec_tree).each { |subtree| yielder.yield subtree }
         end
         dec_tree.pop
       end
