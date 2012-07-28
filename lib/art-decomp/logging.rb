24f604d3aeab88cdc374ee694b0795be1e78a4ad
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-07-28 19:17:01 +0200

    introducing Logging

---
+++ lib/art-decomp/logging.rb
@@ -0,0 +1,2 @@
+module ArtDecomp class Logging
+end end
---
+++ spec/art-decomp/logging_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../spec_helper'
+require_relative '../../lib/art-decomp/logging'
+
+module ArtDecomp describe Logging do
+end end
