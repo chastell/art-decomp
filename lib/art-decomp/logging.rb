e81dee4089df61fea1746fbfa2c1eda798fd8493
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-07-28 19:50:33 +0200

    Logging: trace Executable#run

--- lib/art-decomp/logging.rb
+++ lib/art-decomp/logging.rb
@@ -1,2 +1,19 @@
+require 'logger'
+require 'rcapture'
+
 module ArtDecomp class Logging
+  def initialize level, opts
+    self.logger  = Logger.new $stdout
+    logger.level = Logger::UNKNOWN
+
+    executable = opts.fetch :executable
+    executable.class_eval { include RCapture::Interceptable }
+    executable.capture_pre methods: :run do |point|
+      logger.unknown 'Executable#run'
+    end
+  end
+
+  private
+
+  attr_accessor :logger
 end end
--- spec/art-decomp/logging_spec.rb
+++ spec/art-decomp/logging_spec.rb
@@ -2,4 +2,14 @@ require_relative '../spec_helper'
 require_relative '../../lib/art-decomp/logging'
 
 module ArtDecomp describe Logging do
+  let(:executable) { Class.new { def run; end } }
+
+  describe '.new' do
+    it 'logs Executable#run starts' do
+      capture_io do
+        Logging.new 'unknown', executable: executable
+        executable.new.run
+      end.first.must_include 'Executable#run'
+    end
+  end
 end end
