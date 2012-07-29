7d2d2ca06f0f6f062f64719ce68b0b47d71ac669
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-07-29 06:45:55 +0200

    refactor Logging

--- lib/art-decomp/logging.rb
+++ lib/art-decomp/logging.rb
@@ -6,14 +6,23 @@ module ArtDecomp class Logging
     self.logger  = Logger.new $stdout
     logger.level = Logger::UNKNOWN
 
-    executable = opts.fetch :executable
-    executable.class_eval { include RCapture::Interceptable }
-    executable.capture_pre methods: :run do |point|
-      logger.unknown 'Executable#run'
-    end
+    classes[:executable] = opts.fetch :executable
+
+    log_executable
   end
 
   private
+
+  def classes
+    @classes ||= {}
+  end
+
+  def log_executable
+    classes[:executable].class_eval { include RCapture::Interceptable }
+    classes[:executable].capture_pre methods: :run do |point|
+      logger.unknown 'Executable#run'
+    end
+  end
 
   attr_accessor :logger
 end end
