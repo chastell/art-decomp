8c08157f60a72a79531b212769de4d17e81ad6a7
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-08-19 19:33:09 +0200

    drop CoreExtensions::File.dump_object

--- lib/art-decomp/core_extensions.rb
+++ lib/art-decomp/core_extensions.rb
@@ -1,15 +1,10 @@
 require_relative 'core_extensions/enumerable'
-require_relative 'core_extensions/file'
 require_relative 'core_extensions/integer'
 require_relative 'core_extensions/set'
 require_relative 'core_extensions/string'
 
 class Array
   include ArtDecomp::CoreExtensions::Enumerable
-end
-
-class File
-  extend ArtDecomp::CoreExtensions::File::ClassMethods
 end
 
 class Integer
--- lib/art-decomp/core_extensions/file.rb
+++
@@ -1,7 +0,0 @@
-module ArtDecomp module CoreExtensions module File
-  module ClassMethods
-    def dump_object object, path
-      open(path, 'w') { |file| Marshal.dump object, file }
-    end
-  end
-end end end
