6f026887b77b6327ce476de759cd88cbbc0f96ee
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-08-19 10:52:30 +0200

    Blanket: use attr_reader

--- lib/art-decomp/blanket.rb
+++ lib/art-decomp/blanket.rb
@@ -20,17 +20,17 @@ module ArtDecomp class Blanket
   end
 
   def * other
-    ints = []
-    @ints.each do |this_int|
+    new_ints = []
+    ints.each do |this_int|
       other.ints.each do |other_int|
-        ints << (this_int & other_int)
+        new_ints << (this_int & other_int)
       end
     end
-    Blanket.new ints
+    Blanket.new new_ints
   end
 
   def == other
-    @ints == other.ints
+    ints == other.ints
   end
 
   def encoding bits
@@ -40,34 +40,34 @@ module ArtDecomp class Blanket
   end
 
   def encodings bits
-    sorted = @ints.sort
+    sorted = ints.sort
     width = sorted.size.log2_ceil
-    encs = @ints.select { |int| int & bits == bits }.map { |int| sorted.index(int) }.map { |i| i.to_s(2).rjust width, '0' }
-    encs.size == 0 or encs.size == @ints.size ? [DontCare.to_s * width] : encs
+    encs = ints.select { |int| int & bits == bits }.map { |int| sorted.index(int) }.map { |i| i.to_s(2).rjust width, '0' }
+    encs.size == 0 or encs.size == ints.size ? [DontCare.to_s * width] : encs
   end
 
   alias eql? ==
 
   def hash
-    @ints.hash
+    ints.hash
   end
 
   def inspect
-    "Blanket[#{@ints.map(&:bits).sort.map { |bl| "B[#{bl.join ','}]" }.join ', '}]"
+    "Blanket[#{ints.map(&:bits).sort.map { |bl| "B[#{bl.join ','}]" }.join ', '}]"
   end
 
   def pins
-    @ints.size.log2_ceil
+    ints.size.log2_ceil
   end
 
   def seps
-    potential = @ints.inject(0, :|).bits.pairs.map { |a, b| Sep[a,b] }
-    not_separate = @ints.map{ |i| i.bits.pairs.map { |a, b| Sep[a,b] } }.flatten
+    potential = ints.inject(0, :|).bits.pairs.map { |a, b| Sep[a,b] }
+    not_separate = ints.map{ |i| i.bits.pairs.map { |a, b| Sep[a,b] } }.flatten
     (potential - not_separate).to_set
   end
 
   def size
-    @ints.size
+    ints.size
   end
 
   alias to_s inspect
