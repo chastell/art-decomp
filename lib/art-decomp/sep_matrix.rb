1f4f465a278f59ec4b0bc8efbfae0bbacd1e987c
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-08-22 09:48:32 +0200

    first cut of SepMatrix.from_blanket

--- lib/art-decomp/sep_matrix.rb
+++ lib/art-decomp/sep_matrix.rb
@@ -3,6 +3,27 @@ module ArtDecomp class SepMatrix
     @matrix = matrix
   end
 
+  def self.from_blanket blanket
+    size = blanket.ints.max.to_s(2).size
+    ones = (1 << size) - 1
+    matrix = Array.new size, ones
+
+    blanket.ints.each do |int|
+      int.bits.pairs.each do |a, b|
+        mask = 1 << a | 1 << b
+        matrix[a] ^= mask
+        matrix[b] ^= mask
+      end
+    end
+
+    matrix.each_index do |i|
+      mask = 1 << i
+      matrix[i] ^= mask unless matrix[i][i].zero?
+    end
+
+    new matrix
+  end
+
   def == other
     @matrix == other.matrix
   end
--- spec/art-decomp/sep_matrix_spec.rb
+++ spec/art-decomp/sep_matrix_spec.rb
@@ -1,6 +1,19 @@
 require_relative '../spec_helper'
 
 module ArtDecomp describe SepMatrix do
+  describe '.from_blanket' do
+    it 'builds upon separations provided by a Blanket' do
+      blanket = Blanket[B[0], B[1,2], B[2,3,4]]
+      SepMatrix.from_blanket(blanket).must_equal SepMatrix.new [
+        0b11110,
+        0b11001,
+        0b00001,
+        0b00011,
+        0b00011,
+      ]
+    end
+  end
+
   describe '#==' do
     it 'compares SepMatrices properly' do
       SepMatrix.new([]).must_equal SepMatrix.new []
