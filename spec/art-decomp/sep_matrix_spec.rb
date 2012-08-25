e09c6b57b31b33575f925735721e3ab1db5cfa06
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-08-25 11:14:48 +0200

    drop SepMatrix#seps to escape the lure of Sets of Seps

--- spec/art-decomp/sep_matrix_spec.rb
+++ spec/art-decomp/sep_matrix_spec.rb
@@ -59,15 +59,4 @@ module ArtDecomp describe SepMatrix do
       ]
     end
   end
-
-  describe '#seps' do
-    it 'returns provided Seps' do
-      SepMatrix.new([
-        0b1000,
-        0b0100,
-        0b0010,
-        0b0001,
-      ]).seps.must_equal Set[Sep[0,3], Sep[1,2]]
-    end
-  end
 end end
