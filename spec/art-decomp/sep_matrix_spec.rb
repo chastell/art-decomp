fa1a665dcd0fffae43e1429256ea60e62266ed38
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-08-26 20:34:32 +0200

    bring SepMatrix#seps back

--- spec/art-decomp/sep_matrix_spec.rb
+++ spec/art-decomp/sep_matrix_spec.rb
@@ -134,4 +134,15 @@ module ArtDecomp describe SepMatrix do
       refute sep_01_12.superset? sep_01_02
     end
   end
+
+  describe '#seps' do
+    it 'returns provided Seps' do
+      SepMatrix.new([
+        0b1000,
+        0b0100,
+        0b0010,
+        0b0001,
+      ]).seps.must_equal Set[Sep[0,3], Sep[1,2]]
+    end
+  end
 end end
