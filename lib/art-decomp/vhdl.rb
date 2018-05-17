c423bda247bc0e92ddf07d8a8e92a7fb761c5015
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2018-05-17 21:37:05 +0200

    RuboCop: fix IndentHeredoc

--- lib/art-decomp/vhdl.rb
+++ lib/art-decomp/vhdl.rb
@@ -42,29 +42,29 @@ module ArtDecomp class VHDL
       states << "  signal current_state, next_state: std_logic_vector(#{@fsm.codes.first.last.size - 1} downto 0);"
       default_state = "\"#{'-' * @fsm.codes.values.first.size}\""
     end
-    <<-VHDL
-library ieee;
-use ieee.numeric_std.all;
-use ieee.std_logic_1164.all;
-entity #{name} is
-  port(
-    clock:  in  std_logic;
-    input:  in  std_logic_vector(#{structure.first.last.first.first.size - 1} downto 0);
-    output: out std_logic_vector(#{structure.first.last.first.last[:output].size - 1} downto 0)
-  );
-end #{name};
-architecture behaviour of #{name} is
-#{states.join "\n"}
-begin
-  process(clock) begin
-    if rising_edge(clock) then current_state <= next_state;
-    end if;
-  end process;
-  process(input, current_state) begin
-    next_state <= #{default_state}; output <= "#{'-' * structure.first.last.first.last[:output].size}";
-#{logic.join "\n"}
-  end process;
-end behaviour;
+    <<~VHDL
+      library ieee;
+      use ieee.numeric_std.all;
+      use ieee.std_logic_1164.all;
+      entity #{name} is
+        port(
+          clock:  in  std_logic;
+          input:  in  std_logic_vector(#{structure.first.last.first.first.size - 1} downto 0);
+          output: out std_logic_vector(#{structure.first.last.first.last[:output].size - 1} downto 0)
+        );
+      end #{name};
+      architecture behaviour of #{name} is
+      #{states.join "\n"}
+      begin
+        process(clock) begin
+          if rising_edge(clock) then current_state <= next_state;
+          end if;
+        end process;
+        process(input, current_state) begin
+          next_state <= #{default_state}; output <= "#{'-' * structure.first.last.first.last[:output].size}";
+      #{logic.join "\n"}
+        end process;
+      end behaviour;
     VHDL
   end
 end end
