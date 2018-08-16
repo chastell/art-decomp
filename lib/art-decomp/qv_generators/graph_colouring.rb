e99d915333c4156b7c4efb3d002173c4545bb5ee
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2018-08-16 10:10:05 +0200

    RuboCop: work on LineLength

--- lib/art-decomp/qv_generators/graph_colouring.rb
+++ lib/art-decomp/qv_generators/graph_colouring.rb
@@ -5,14 +5,16 @@ module ArtDecomp
         self.class == other.class
       end
 
-      def blankets(fsm, u, v, beta_qu) # rubocop:disable AbcSize
+      def blankets(fsm, u, v, beta_qu) # rubocop:disable AbcSize, MethodLength
         beta_u = fsm.beta_x u
         beta_v = fsm.beta_x v
         Enumerator.new do |yielder|
           beta_g  = Graph.new(fsm.beta_q * beta_v, fsm.beta_f.seps - beta_u.seps - beta_qu.seps).blanket_from_colouring
           beta_qv = Graph.new(fsm.beta_q, beta_g.seps - beta_v.seps).blanket_from_colouring
           yielder.yield beta_qv, beta_g
-          beta_qv = Graph.new(fsm.beta_q, fsm.beta_f.seps - beta_u.seps - beta_qu.seps - beta_v.seps).blanket_from_colouring
+          qv_graph = Graph.new(fsm.beta_q,
+                               fsm.beta_f.seps - beta_u.seps - beta_qu.seps - beta_v.seps)
+          beta_qv = qv_graph.blanket_from_colouring
           beta_g  = Graph.new(beta_qv * beta_v, fsm.beta_f.seps - beta_u.seps - beta_qu.seps).blanket_from_colouring
           yielder.yield beta_qv, beta_g
         end
